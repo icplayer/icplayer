6cc0997346fc6019e7adbd0739372a028cba61e8
Krzysztof Langner <klangner@gmail.com>
2014-03-18 09:02:51 +0100

    Added getAllPages to Chapter interface.

--- src/main/java/com/lorepo/icplayer/client/module/api/player/IChapter.java
+++ src/main/java/com/lorepo/icplayer/client/module/api/player/IChapter.java
@@ -1,9 +1,14 @@
 package com.lorepo.icplayer.client.module.api.player;
 
+import java.util.List;
+
+import com.lorepo.icplayer.client.model.Page;
+
 
 
 public interface IChapter extends IContentNode {
 	boolean add(IContentNode node);
 	public IContentNode get(int index);
 	public int size();
+	public List<Page> getAllPages();
 }
