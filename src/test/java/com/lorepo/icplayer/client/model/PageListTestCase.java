145f0ddc9a44fed17a0873f29e7cc2b5e5cbde03
Krzysztof Langner <klangner@gmail.com>
2014-03-19 15:16:58 +0100

    Removed unused import

--- src/test/java/com/lorepo/icplayer/client/model/PageListTestCase.java
+++ src/test/java/com/lorepo/icplayer/client/model/PageListTestCase.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
