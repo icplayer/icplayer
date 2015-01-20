e3569ae55d9ff5d90fb25d36a8ac1f9ea59ff2d2
rluczyk <romuald.luczyk@solwit.com>
2015-01-20 09:22:27 +0100

    [Text] Fixed issue with ignore punctuation and accented letters in Text module

--- src/main/java/com/lorepo/icplayer/client/module/text/GapInfo.java
+++ src/main/java/com/lorepo/icplayer/client/module/text/GapInfo.java
@@ -6,21 +6,16 @@ import java.util.List;
 
 import com.lorepo.icf.utils.StringUtils;
 
-/**
- * Zawiera informacje o gapie
- * @author Krzysztof Langner
- *
- */
 public class GapInfo {
 
 	private String id;
 	private List<String> answers = new ArrayList<String>();
+	private List<String> checkAnswers = new ArrayList<String>();
 	private int	value;
 	private int maxLength;
 	private boolean isCaseSensitive = false;
 	private boolean isIgnorePunctuation;
 	private String placeHolder = "";
-	
 	
 	public GapInfo(String id, int value, boolean isCaseSensitive, boolean isIgnorePunctuation, int maxLength){
 		this.id = id;
@@ -33,12 +28,50 @@ public class GapInfo {
 
 	public void addAnswer(String answer) {
 		answer = StringUtils.unescapeXML(answer);
-		if(isIgnorePunctuation) { answer = removePunctuation(answer); }
+		answer = answer.replaceAll("&nbsp;", " ");
+		
 		answers.add(isCaseSensitive ? answer : answer.toLowerCase());
+		if(isIgnorePunctuation) { answer = removePunctuation(answer); }
+		checkAnswers.add(isCaseSensitive ? answer : answer.toLowerCase());
 	}
 
+	public static boolean isLetter(char c) {
+        int character = (int) c;
+
+        return inRange(character, 65, 90) || inRange(character, 97, 122) || inRange(character, 192, 687) || inRange(character, 900, 1159) || // latin letters
+		       inRange(character, 1162, 1315) || inRange(character, 1329, 1366) || inRange(character, 1377, 1415) || // cyrillic letters
+		       inRange(character, 1425, 1536) || inRange(character, 1569, 1610) || // arabic letters
+		       inRange(character, 0x3400, 0x9FFF) || inRange(character, 0x0620, 0x063F) || inRange(character, 0x0641, 0x064A); //chinese and japanese letters
+    }
+	
+	public static boolean isDigit(int d) {
+		
+        return inRange(d, 0x0030, 0x0039) //standard european digits
+        		|| inRange(d, 0x0660, 0x0669) || inRange(d, 0x06F0, 0x06F9) // arabic digits
+        		|| inRange(d, 0x1040, 0x108F) || inRange(d, 0x5344, 0x5345) // chinese and japanese digits
+        		|| d == 0x3007 || d == 0x5341 || d == 0x4E00 || d == 0x4E8C || d == 0x4E09 || d == 0x56DB
+        		|| d == 0x4E94 || d == 0x0516D || d == 0x4E03 || d == 0x516B || d == 0x4E5D || d == 0x5341
+        		|| d == 0x767E || d == 0x5343 || d == 0x4E07 || d == 0x842C || d == 0x5104 || d == 0x4EBF || d == 0x5146;
+    }
+
+    public static boolean inRange(int value, int min, int max) {
+        return (value <= max) & (value >= min);
+    }
+	
 	private static String removePunctuation(String text) {
-		return text.replaceAll("\\W", "");
+		String alfaNumericText = "";
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			if (GapInfo.isLetter(c) || GapInfo.isDigit(c)) {
+				sb.append(c);
+			}
+		}
+		
+		alfaNumericText = sb.toString();
+
+		return alfaNumericText;
 	}
 
 	public boolean isCorrect(String text) {
@@ -50,7 +83,7 @@ public class GapInfo {
 		if(isIgnorePunctuation){
 			text = removePunctuation(text);
 		}
-		for(String answer : answers){
+		for(String answer : checkAnswers){
 			if(answer.compareTo(text) == 0){
 				correct = true;
 				break;
