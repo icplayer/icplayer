054e8fd39f5afa46f19c385cbce77c02611a0aba
mmotlawski <miotla007@wp.pl>
2015-01-22 13:16:15 +0100

    [Shape Tracing] Added Show Answers

--- addons/Shape_Tracing/src/presenter.js
+++ addons/Shape_Tracing/src/presenter.js
@@ -65,6 +65,8 @@ function AddonShape_Tracing_create() {
         }
 
         presenter.$view.find(".drawing").removeClass("correct wrong");
+        presenter.setVisibility(presenter.visibleByDefault);
+        presenter.configuration.isVisible = presenter.visibleByDefault;
     }
 
     function eventCreator() {
@@ -202,8 +204,15 @@ function AddonShape_Tracing_create() {
     function colorNameToHex(color) {
         var colors = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff","beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887","cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff","darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f","darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1","darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff","firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff","gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f","honeydew":"#f0fff0","hotpink":"#ff69b4","indianred ":"#cd5c5c","indigo ":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c","lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2","lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de","lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6","magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee","mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5","navajowhite":"#ffdead","navy":"#000080","oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6","palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1","saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4","tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0","violet":"#ee82ee","wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5","yellow":"#ffff00","yellowgreen":"#9acd32"};
 
-        if (typeof colors[color.toLowerCase()] !== 'undefined') {
-            return colors[color.toLowerCase()];
+        var parsedColor;
+        if(Array.isArray(color)){
+            parsedColor = color[0];
+        }else{
+            parsedColor = color;
+        }
+
+        if (typeof colors[parsedColor.toLowerCase()] !== 'undefined') {
+            return colors[parsedColor.toLowerCase()];
         } else {
             return false;
         }
@@ -351,6 +360,32 @@ function AddonShape_Tracing_create() {
         //image.crossOrigin = "anonymous";
         image.src = presenter.configuration.shapeImage;
     }
+
+    function drawCorrectAnswerImage(isPreview) {
+        presenter.stageCorrect = new Kinetic.Stage({
+        container: presenter.data.divID + "_correctImage",
+        height: presenter.data.height,
+        width: presenter.data.width
+        });
+        presenter.correctAnswerlayer = new Kinetic.Layer();
+        var correctImage = new Image();
+        correctImage.onload = function() {
+            var correctImg = new Kinetic.Image({
+            x: 0,
+            y: 0,
+            image: correctImage,
+            height: presenter.data.height,
+            width: presenter.data.width
+        });
+
+        presenter.correctAnswerlayer.add(correctImg);
+        presenter.stageCorrect.add(presenter.correctAnswerlayer);
+
+        calculateBorderCoordinates();
+    };
+    correctImage.src = presenter.configuration.correctAnswerImage;
+    //presenter.correctAnswerlayer.hide();&#13;
+}
 
     function updateCursorPosition(e) {
         presenter.cursorPosition.pre_x = presenter.cursorPosition.x;
@@ -708,6 +743,8 @@ function AddonShape_Tracing_create() {
             return returnErrorObject(validatedBorder.errorCode);
         }
 
+        var correctAnswerImg = model["Correct Answer Image"];
+
         return {
             shapeImage: validatedShapeImage.value,
             isShowShapeImage: ModelValidationUtils.validateBoolean(model["Show Shape image"]),
@@ -721,6 +758,7 @@ function AddonShape_Tracing_create() {
             penThickness: validatedThickness_Pen.value,
             opacity: validatedOpacity.value,
             border: validatedBorder.value,
+            correctAnswerImage: correctAnswerImg,
 
             ID: model["ID"],
             isVisible: ModelValidationUtils.validateBoolean(model['Is Visible']),
@@ -766,6 +804,7 @@ function AddonShape_Tracing_create() {
 
         presenter.$view.find("div.background").attr('id', presenter.data.divID + "_background");
         presenter.$view.find("div.shape").attr('id', presenter.data.divID + "_shape");
+        presenter.$view.find("div.correctImage").attr('id', presenter.data.divID + "_correctImage");
 
         presenter.$view.css('opacity', presenter.configuration.opacity);
 
@@ -773,17 +812,27 @@ function AddonShape_Tracing_create() {
         drawBackGroundImage(isPreview);
         drawShapeImage(isPreview);
 
+        if(presenter.configuration.correctAnswerImage && !isPreview){
+            drawCorrectAnswerImage(isPreview);
+        }
+
         if (!isPreview) {
             turnOnEventListeners();
         }
 
         presenter.setVisibility(presenter.configuration.isVisible);
+        presenter.visibleByDefault = presenter.configuration.isVisible;
+
+        presenter.$view.find('.correctImage').css('display', 'none');
 
         return false;
     };
 
     presenter.run = function(view, model) {
         presenter.presenterLogic(view, model, false);
+
+        eventBus.addEventListener('ShowAnswers', this);
+        eventBus.addEventListener('HideAnswers', this);
     };
 
     presenter.createPreview = function(view, model) {
@@ -813,10 +862,12 @@ function AddonShape_Tracing_create() {
 
     presenter.show = function() {
         presenter.setVisibility(true);
+        presenter.configuration.isVisible = true;
     };
 
     presenter.hide = function() {
         presenter.setVisibility(false);
+        presenter.configuration.isVisible = false;
     };
 
     presenter.executeCommand = function(name, params) {
@@ -825,11 +876,14 @@ function AddonShape_Tracing_create() {
         }
 
         var commands = {
+            "reset": presenter.reset,
             "show": presenter.show,
             "hide": presenter.hide,
             "setEraserOn": presenter.setEraserOn,
-            "setColor": presenter.setColor,
-            "setThickness": presenter.setThickness
+            "setThickness": presenter.setThickness,
+            "showAnswers": presenter.showAnswers,
+            "hideAnswers": presenter.hideAnswers,
+            "setColor": presenter.setColor
         };
 
         Commands.dispatch(commands, name, params, presenter);
@@ -856,6 +910,10 @@ function AddonShape_Tracing_create() {
     }
 
     presenter.setShowErrorsMode = function() {
+        if (presenter.isShowAnswersActive) {
+            presenter.hideAnswers();
+        }
+
         turnOffEventListeners();
 
         if (presenter.data.isStarted) {
@@ -885,6 +943,9 @@ function AddonShape_Tracing_create() {
     };
 
     presenter.reset = function() {
+        if (presenter.isShowAnswersActive) {
+            presenter.hideAnswers();
+        }
         resetAddon(true);
     };
 
@@ -909,10 +970,6 @@ function AddonShape_Tracing_create() {
     };
 
     presenter.getState = function() {
-        if (!presenter.data.isStarted) {
-            return;
-        }
-
         return JSON.stringify({
             imgData: presenter.$view.find(".drawing")[0].toDataURL("image/png"),
             isPencilActive: presenter.data.isPencilActive,
@@ -922,8 +979,23 @@ function AddonShape_Tracing_create() {
             numberOfLines: presenter.data.numberOfLines,
             numberOfDescentsFromShape: presenter.data.numberOfDescentsFromShape,
             isAllPointsChecked: presenter.data.isAllPointsChecked,
-            pointsArray: presenter.pointsArray
+            pointsArray: presenter.pointsArray,
+            isVisible: presenter.configuration.isVisible
         });
+    };
+
+    presenter.upgradeStateForVisibility = function (parsedState) {
+        if (parsedState.isVisible == undefined) {
+            parsedState.isVisible = true;
+        }
+
+        return parsedState;
+    };
+
+    presenter.upgradeState = function (parsedState) {
+        var upgradedState = presenter.upgradeStateForVisibility(parsedState);
+
+        return  upgradedState;
     };
 
     presenter.setState = function(state) {
@@ -931,12 +1003,17 @@ function AddonShape_Tracing_create() {
             return;
         }
 
+        var parsedState = JSON.parse(state);
+
+        parsedState = presenter.upgradeState(parsedState);
+
         presenter.data.isStarted = true; // state is non empty => exercise is started
         presenter.data.currentPointNumber = JSON.parse(state).currentPointNumber;
         presenter.data.numberOfLines = JSON.parse(state).numberOfLines;
         presenter.data.numberOfDescentsFromShape = JSON.parse(state).numberOfDescentsFromShape;
         presenter.data.isAllPointsChecked = JSON.parse(state).isAllPointsChecked;
         presenter.pointsArray = JSON.parse(state).pointsArray;
+        presenter.configuration.isVisible = parsedState.isVisible;
 
         var savedImg = new Image();
         savedImg.onload = function() {
@@ -948,10 +1025,55 @@ function AddonShape_Tracing_create() {
             }
         };
         savedImg.src = JSON.parse(state).imgData;
+        presenter.setVisibility(presenter.configuration.isVisible);
     };
 
     presenter.isAllOk = function() {
         return presenter.data.isAllOk;
+    };
+
+    presenter.onEventReceived = function (eventName) {
+        if (eventName == "ShowAnswers") {
+            presenter.showAnswers();
+        }
+
+        if (eventName == "HideAnswers") {
+            presenter.hideAnswers();
+        }
+    };
+
+    presenter.showAnswers = function () {
+        presenter.isShowAnswersActive = true;
+        presenter.setWorkMode();
+        turnOffEventListeners();
+
+        presenter.$view.find(".drawing").css('display', 'none');
+
+        if(presenter.configuration.correctAnswerImage){
+            presenter.layer.hide();
+            presenter.$view.find('.correctImage').css('display', 'block');
+        }else{
+            presenter.layer.show();
+        }
+
+        presenter.$view.find('.background').addClass('shape-tracing-show-answers');
+    };
+
+    presenter.hideAnswers = function () {
+        presenter.$view.find('.background').removeClass('shape-tracing-show-answers');
+
+        if(presenter.correctAnswerlayer){
+            presenter.$view.find('.correctImage').css('display', 'none');
+        }
+        if (presenter.configuration.isShowShapeImage) {
+            presenter.layer.show();
+        }else{
+            presenter.layer.hide();
+        }
+        presenter.$view.find(".drawing").css('display', 'block');
+        turnOffEventListeners();
+        turnOnEventListeners();
+        presenter.isShowAnswersActive = false;
     };
 
     return presenter;
