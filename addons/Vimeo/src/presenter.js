1ff414bae4f85b9c231311be23501c89c919653b
miotla007 <miotla007@wp.pl>
2016-07-19 10:41:31 +0200

    [Vimeo] Fixed issue with https

--- addons/Vimeo/src/presenter.js
+++ addons/Vimeo/src/presenter.js
@@ -6,13 +6,14 @@ function AddonVimeo_create(){
 		var iframe = view.getElementsByTagName('iframe')[0];
 		var src = iframe.getAttribute('src');
 		src = src.replace('${movie id}', model['movie id'])
+        src = src.replace('http', 'https');
 		iframe.setAttribute('src', src);
 	}
 
 	presenter.createPreview = function(view, model){
 		$.ajax({
 	        type:'GET',
-	        url: 'http://vimeo.com/api/v2/video/' + model['movie id'] + '.json',
+	        url: 'https://vimeo.com/api/v2/video/' + model['movie id'] + '.json',
 	        jsonp: 'callback',
 	        dataType: 'jsonp',
 	        success: function(data){
