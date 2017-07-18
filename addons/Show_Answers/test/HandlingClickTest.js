2d3add12d463681552fb75865bd452b1ed64070c
tkrassowski <tomasz.krassowski@solwit.com>
2017-07-18 15:00:46 +0200

    Proper tests

--- addons/Show_Answers/test/HandlingClickTest.js
+++ addons/Show_Answers/test/HandlingClickTest.js
@@ -1,20 +1,18 @@
-TestCase('[Show_Answers] Handling clicks', {
-    setUp: function () {
+(function () {
+    function setUpPresenter() {
         this.presenter = AddonShow_Answers_create();
+
+        this.commands = {
+            incrementCheckCounter: sinon.stub(),
+            increaseMistakeCounter: sinon.stub()
+        };
 
         this.stubs = {
             addClassStub: sinon.stub(),
             removeClassStub: sinon.stub(),
             textStub: sinon.stub(),
             sendEventStub: sinon.stub(this.presenter, 'sendEvent'),
-            incrementCheckCounterStub: sinon.stub(),
-            incrementMistakeCounterStub: sinon.stub(),
             getCommandsStubs: sinon.stub().returns(this.commands)
-        };
-
-        this.commands = {
-            incrementCheckCounter:  this.stubs.incrementCheckCounterStub,
-            increaseMistakeCounter:  this.stubs.incrementMistakeCounterStub
         };
 
         this.presenter.playerController = {
@@ -32,20 +30,107 @@ TestCase('[Show_Answers] Handling clicks', {
             enableCheckCounter: false,
             enableMistakeCounter: false
         };
-
-        this.button = document.createElement('div');
+        this.button = document.createElement("div");
         this.button.className = 'show-answers-button';
 
-        this.presenter.$button = $(".show-answers-button");
-    },
-    
-    tearDown: function () {
-        document.body.removeChild(this.presenter.button);
-    },
+        this.presenter.$button = jQuery(this.button);
+    };
 
-    'test should call sendEvent once': function () {
-        this.$button.trigger('click');
+    TestCase('[Show_Answers] Basic code flow', {
+        setUp: setUpPresenter,
 
-        assertTrue(this.stubs.sendEventStub.calledOnce);
-    }
-});
+        'test should call sendEvent once': function () {
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertTrue(this.stubs.sendEventStub.calledOnce);
+        },
+
+        'test should call addClass when isSelected is false': function () {
+            this.presenter.configuration.isSelected = false;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertTrue(this.stubs.addClassStub.calledOnce);
+        },
+
+        'test should call removeClass when isSelected is true': function () {
+            this.presenter.configuration.isSelected = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertTrue(this.stubs.removeClassStub.calledOnce);
+        }
+    });
+
+    TestCase('[Show_Answers] Check counter', {
+        setUp: setUpPresenter,
+
+        'test should call incrementCheckCounter when isSelected is false and enableCheckCounter is true': function () {
+            this.presenter.configuration.isSelected = false;
+            this.presenter.configuration.enableCheckCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertTrue(this.commands.incrementCheckCounter.calledOnce);
+        },
+
+        'test should not call incrementCheckCounter when isSelected is true and enableCheckCounter is false': function () {
+            this.presenter.configuration.isSelected = true;
+            this.presenter.configuration.enableCheckCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertFalse(this.commands.incrementCheckCounter.calledOnce);
+        },
+
+        'test should not call incrementCheckCounter when isSelected is true and enableCheckCounter is true': function () {
+            this.presenter.configuration.isSelected = true;
+            this.presenter.configuration.enableCheckCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertFalse(this.commands.incrementCheckCounter.calledOnce);
+        }
+    });
+
+    TestCase('[Show_Answers] Mistake counter', {
+        setUp: setUpPresenter,
+
+        'test should call incrementMistakeCounter when isSelected is false and enableMistakeCounter is true': function () {
+            this.presenter.configuration.isSelected = false;
+            this.presenter.configuration.enableMistakeCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertTrue(this.commands.increaseMistakeCounter.calledOnce);
+        },
+
+        'test should not call incrementMistakeCounter when isSelected is true and enableMistakeCounter is false': function () {
+            this.presenter.configuration.isSelected = true;
+            this.presenter.configuration.enableMistakeCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertFalse(this.commands.increaseMistakeCounter.calledOnce);
+        },
+
+        'test should not call incrementMistakeCounter when isSelected is true and enableMistakeCounter is true': function () {
+            this.presenter.configuration.isSelected = true;
+            this.presenter.configuration.enableMistakeCounter = true;
+
+            this.presenter.handleClickAction();
+            this.presenter.$button.trigger('click');
+
+            assertFalse(this.commands.increaseMistakeCounter.calledOnce);
+        }
+    });
+
+})();
