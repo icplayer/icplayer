148c5889704ea075eab0af55905ee228abe057ce
apietrzak <adam.pietrzak@solwit.com>
2017-08-25 10:35:32 +0200

    [Image ID] Added WCAG support RFD #6129

--- src/main/java/com/lorepo/icplayer/client/page/KeyboardNavigationController.java
+++ src/main/java/com/lorepo/icplayer/client/page/KeyboardNavigationController.java
@@ -39,7 +39,7 @@ public final class KeyboardNavigationController {
 
 	private enum ExpectedModules {
 		// Navigation modules
-		text, video, button, navigation_bar, choice, show_answers, checkbutton, truefalse, gamememo, sourcelist, double_state_button, single_state_button, ordering, connection, multiplegap;
+		text, video, button, navigation_bar, choice, show_answers, checkbutton, truefalse, gamememo, sourcelist, double_state_button, single_state_button, ordering, connection, multiplegap, text_identification;
 		
 		private static boolean contains(String s) {
 			s = s.replaceAll("\\s","");
