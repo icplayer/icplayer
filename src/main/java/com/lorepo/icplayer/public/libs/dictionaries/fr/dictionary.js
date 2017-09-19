cfbfd1521b6295d970bd7708be996704d1a808f0
tkrassowski <tomasz.krassowski@solwit.com>
2017-09-19 09:26:26 +0200

    Added new notification about setting header/footer. RFD #6158

--- src/main/java/com/lorepo/icplayer/public/libs/dictionaries/fr/dictionary.js
+++ src/main/java/com/lorepo/icplayer/public/libs/dictionaries/fr/dictionary.js
@@ -1508,5 +1508,6 @@ var ice_dictionary_fr = {
 "Footer": "Footer",
 "Footer_none": "None",
 "Footer_default": "Default",
-"Cant_add_page_from_template": "One of the pages from the template couldn't be loaded. Importing the template aborted."
+"Cant_add_page_from_template": "One of the pages from the template couldn't be loaded. Importing the template aborted.",
+    "header_footer_notification": "New feature! Now you can define and select a different header or footer <br/> for each lesson page. Having set the additional header/footer pages <br/> in Commons, simply check a relevant option in the Page properties."
 };
