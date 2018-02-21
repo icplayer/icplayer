b0a4c0407aeaa89914a2c5d1b92b76b71c5b1439
apietrzak <adam.pietrzak@solwit.com>
2018-02-21 10:05:51 +0100

    Poprawki

--- addons/Board_Game/test/UpgradeModel.js
+++ addons/Board_Game/test/UpgradeModel.js
@@ -3,11 +3,23 @@ TestCase("[Board Game] Upgrade model Test Case", {
     setUp: function () {
         this.presenter = AddonBoard_Game_create();
 
+        this.exampleModel = {Fields: [{},{},{}]};
+
     },
 
     'test upgradeModel will add missing values': function () {
-        
-    },
+        var upgradedModel = this.presenter.upgradeModel(this.exampleModel);
+
+        assertEquals({
+            isDisabled: false,
+            gameMode: "Free",
+            Fields: [
+                {cssClass: ""},
+                {cssClass: ""},
+                {cssClass: ""}
+            ]
+        }, upgradedModel);
+    }
 
 
 });
