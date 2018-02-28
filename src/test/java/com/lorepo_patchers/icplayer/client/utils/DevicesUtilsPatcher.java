ce27f422625d863e9e4203339d14debc21c55a0f
apietrzak <adam.pietrzak@solwit.com>
2018-02-28 11:27:21 +0100

    Fixed problem with hover in iOS RFD #6353

---
+++ src/test/java/com/lorepo_patchers/icplayer/client/utils/DevicesUtilsPatcher.java
@@ -0,0 +1,13 @@
+package com.lorepo_patchers.icplayer.client.utils;
+
+import com.googlecode.gwt.test.patchers.PatchClass;
+import com.googlecode.gwt.test.patchers.PatchMethod;
+import com.lorepo.icplayer.client.utils.DevicesUtils;
+
+@PatchClass(DevicesUtils.class)
+public class DevicesUtilsPatcher {
+	@PatchMethod
+	public static boolean isSafariMobile() {
+		return false;
+	}
+}
