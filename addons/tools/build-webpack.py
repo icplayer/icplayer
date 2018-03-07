9690e7cede674414858b1b39cbfe7ec71ed8976a
apietrzak <adam.pietrzak@solwit.com>
2018-03-07 14:32:27 +0100

    Poprawki testow

--- addons/tools/build-webpack.py
+++ addons/tools/build-webpack.py
@@ -1,20 +1,21 @@
 import subprocess
 from sys import platform
 
+
 def build_linux():
     p = subprocess.Popen(["npm install"], shell=True)
     p.wait()
-    p = subprocess.Popen(["./node_modules/.bin/webpack"], shell=True)
-    p.wait()
+
 
 def build_windows():
     p = subprocess.Popen(["npm", "install"], shell=True)
     p.wait()
-    p = subprocess.Popen(["./node_modules/.bin/webpack"], shell=True)
-    p.wait()
+
 
 if platform == "linux" or platform == "linux2":
     build_linux()
 else:
     build_windows()
 
+p = subprocess.Popen(["./node_modules/.bin/webpack"], shell=True)
+p.wait()
