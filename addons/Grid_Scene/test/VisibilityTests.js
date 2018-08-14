a3b80f1a8132b9d30eb115e35f5b8e98b5d0aa23
Tomasz Krassowski <tomasz.krassowski@solwit.com>
2018-08-14 13:05:34 +0200

    Testy do grid scene i graph

--- addons/Grid_Scene/test/VisibilityTests.js
+++ addons/Grid_Scene/test/VisibilityTests.js
@@ -1,58 +1,66 @@
-TestCase('[Graph] Visibility tests', {
+TestCase('[Grid_Scene] Visibility tests', {
     setUp: function () {
-        this.presenter = Addongraph_create();
+        this.presenter = AddonGrid_Scene_create();
 
         this.stubs = {
             validateModelStub: sinon.stub(),
             setVisibilityStub: sinon.stub(),
-            upgradeModelStub: sinon.stub(),
-            drawGraphStub: sinon.stub()
+            setQueLoopTimerStub: sinon.stub(),
+            saveAnswerStub: sinon.stub()
         };
 
         this.presenter.validateModel = this.stubs.validateModelStub;
         this.presenter.setVisibility = this.stubs.setVisibilityStub;
-        this.presenter.upgradeModel = this.stubs.upgradeModelStub;
-        this.presenter.drawGraph = this.stubs.drawGraphStub;
+        this.presenter.setQueLoopTimer = this.stubs.setQueLoopTimerStub;
+        this.presenter.saveAnswer = this.stubs.saveAnswerStub;
 
         this.view = document.createElement('div');
     },
 
     'test when in preview mode, setVisibility should be called with true': function () {
         this.stubs.validateModelStub.returns({
-            isValid: true,
-            isVisible: true
+            isError: false,
+            isVisible: true,
+            rows: 0,
+            columns: 0
         });
-        this.presenter.initialize(this.view, {}, true);
+        this.presenter.presenterLogic(this.view, {}, true);
 
         assertTrue(this.stubs.setVisibilityStub.calledWith(true));
     },
 
     'test when in preview mode and addon is not visible, setVisibility should be called with true': function () {
         this.stubs.validateModelStub.returns({
-            isValid: true,
-            isVisible: false
+            isError: false,
+            isVisible: false,
+            rows: 0,
+            columns: 0
         });
-        this.presenter.initialize(this.view, {}, true);
+        this.presenter.presenterLogic(this.view, {}, true);
 
         assertTrue(this.stubs.setVisibilityStub.calledWith(true));
     },
 
     'test when not in preview mode and addon is visible, setVisibility should be called with true': function () {
         this.stubs.validateModelStub.returns({
-            isValid: true,
-            isVisible: true
+            isError: false,
+            isVisible: true,
+            rows: 0,
+            columns: 0
         });
-        this.presenter.initialize(this.view, {}, true);
+        this.presenter.presenterLogic(this.view, {}, false);
 
-        assertTrue(this.stubs.setVisibilityStub.calledWith(false));
+        assertTrue(this.stubs.setVisibilityStub.calledWith(true));
     },
 
     'test when not in preview mode and addon is not visible, setVisibility should be called with false': function () {
         this.stubs.validateModelStub.returns({
-            isValid: true,
-            isVisible: false
+            isError: false,
+            isVisible: false,
+            rows: 0,
+            columns: 0
         });
-        this.presenter.initialize(this.view, {}, true);
+        this.presenter.presenterLogic(this.view, {}, false);
 
         assertTrue(this.stubs.setVisibilityStub.calledWith(false));
     }
