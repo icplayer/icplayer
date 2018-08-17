3b7634b5ec3289f5aecb7385e34f336b49d14625
Tomasz Krassowski <tomasz.krassowski@solwit.com>
2018-08-17 11:00:13 +0200

    [Double_State_Button][Single_State_Button] Fixed keyboard tests.

--- addons/Double_State_Button/test/KeyboardTests.js
+++ addons/Double_State_Button/test/KeyboardTests.js
@@ -2,36 +2,42 @@ TestCase("[Double_State_Button] Handling keyboard events test case", {
     setUp: function () {
         this.presenter = AddonDouble_State_Button_create();
         this.stubs = {
-            'clickHandlerStub': sinon.stub()
+            'clickHandlerStub': sinon.stub(),
+            'preventDefaultStub': sinon.stub()
         };
 
         this.presenter.clickHandler = this.stubs.clickHandlerStub;
+
+        this.event = {
+            preventDefault: this.stubs.preventDefaultStub,
+            keyCode: 0
+        };
 
         this.presenter.wrapper = document.createElement('div');
         this.presenter.addKeyboardListeners();
     },
 
     'test when event keycode is space it should execute click logic': function () {
-        var event = new KeyboardEvent('keydown', {keyCode: 32});
+        this.event.keyCode = 32;
 
-        this.presenter.wrapper.dispatchEvent(event);
+        this.presenter.handleKeyboardEvents(this.event);
 
         assertTrue(this.stubs.clickHandlerStub.called);
+        assertTrue(this.stubs.preventDefaultStub.called);
     },
 
     'test when event keycode is enter it should execute click logic': function () {
-         var event = new KeyboardEvent('keydown', {keyCode: 13});
-
-        this.presenter.wrapper.dispatchEvent(event);
+        this.event.keyCode = 13;
+        this.presenter.handleKeyboardEvents(this.event);
 
         assertTrue(this.stubs.clickHandlerStub.called);
+        assertTrue(this.stubs.preventDefaultStub.called);
     },
 
     'test when event keycode is not enter nor space it should not execute click logic': function () {
-         var event = new KeyboardEvent('keydown', {keyCode: 355});
-
-        this.presenter.wrapper.dispatchEvent(event);
+        this.presenter.handleKeyboardEvents(this.event);
 
         assertFalse(this.stubs.clickHandlerStub.called);
+        assertFalse(this.stubs.preventDefaultStub.called);
     }
 });
