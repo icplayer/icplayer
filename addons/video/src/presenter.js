a8b9b11ef1fcef992e338e0a5ea1766c987f3111
miotla <mateusz.motlawski@solwit.com>
2018-08-24 14:25:32 +0200

    [Video] Fixed problem with hiding video descriptions

--- addons/video/src/presenter.js
+++ addons/video/src/presenter.js
@@ -1483,10 +1483,13 @@ function Addonvideo_create() {
      * @param caption - used text, top and left properties
      * @return reference do newly created element
      */
-    function createCaptionElement(caption) {
+    function createCaptionElement(caption, isAudioDescription) {
         var captionElement = document.createElement('div');
 
         $(captionElement).addClass('captions');
+        if(isAudioDescription) {
+            $(captionElement).addClass('audio-description');
+        }
         $(captionElement).addClass(caption.cssClass);
         $(captionElement).html(caption.text);
         $(captionElement).css({
@@ -1516,7 +1519,7 @@ function Addonvideo_create() {
                     text: parts[5]
                 };
 
-                caption.element = createCaptionElement(caption);
+                caption.element = createCaptionElement(caption, false);
                 presenter.captions.push(caption);
 
                 presenter.captionDivs.push(caption.element);
@@ -1581,7 +1584,7 @@ function Addonvideo_create() {
                     text: parts[5]
                 };
 
-                description.element = createCaptionElement(description);
+                description.element = createCaptionElement(description, true);
                 presenter.descriptions.push(description);
 
                 presenter.descriptionsDivs.push(description.element);
@@ -1627,12 +1630,12 @@ function Addonvideo_create() {
     };
 
     presenter.showSubtitles = function () {
-        presenter.$view.find('.captions').show();
+        presenter.$view.find('.captions:not(.audio-description)').show();
         presenter.areSubtitlesHidden = false;
     };
 
     presenter.hideSubtitles = function () {
-        presenter.$view.find('.captions').hide();
+        presenter.$view.find('.captions:not(.audio-description)').hide();
         presenter.areSubtitlesHidden = true;
     };
 
