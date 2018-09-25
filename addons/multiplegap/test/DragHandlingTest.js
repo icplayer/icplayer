78de93bde98eb73742fc915210373a12f670cf8f
Maciej Zawłocki <maciej.zawlocki@solwit.com>
2018-09-25 09:35:28 +0200

    Added test of adding dragging class on helper creation

--- addons/multiplegap/test/DragHandlingTest.js
+++ addons/multiplegap/test/DragHandlingTest.js
@@ -12,27 +12,47 @@ TestCase("[Multiple Gap] Draggable", {
                             '<img class="contents" alt="" lang="" src="/file/serve/5954688688062464" style="width: 140px; height: 70px; position: absolute; left: -4px; top: -4px;">' +
                             '<div class="handler" style="color: rgba(0, 0, 0, 0); font-size: 1px; display: block;"></div>' +
                         '</div>' +
-                        '<div class="placeholder ui-draggable dragging" draggablevalue="/file/serve/5391738734641152" draggableitem="200_2" draggabletype="image" style="width: 140px; height: 70px; top: 0px; left: 166px;">' +
+                        '<div class="placeholder ui-draggable" draggablevalue="/file/serve/5391738734641152" draggableitem="200_2" draggabletype="image" style="width: 140px; height: 70px; top: 0px; left: 166px;">' +
                             '<img class="contents" alt="" lang="" src="/file/serve/5391738734641152" style="width: 140px; height: 70px; position: absolute; left: -4px; top: -4px;">' +
                             '<div class="handler" style="color: rgba(0,0,0,0.0); font-size:1px"></div>' +
                         '</div>' +
                     '</div>' +
                 '</div>' +
             '</div>');
+
+        var self = this;
+        this.presenter.$view.find('.placeholder').each(function(){
+           self.presenter.makePlaceholderDraggable($(this));
+        });
+
     },
 
     'test correct events sent on drag stop': function() {
-        var placeholder = this.presenter.$view.find('.placeholder').first();
+        var placeholder = $(this.presenter.$view.find('.placeholder')[0]);
+        
         var stubs = {};
-
         stubs.sendEvent = sinon.spy();
         stubs.movePlaceholdersAfterRemove = sinon.stub( this.presenter, 'movePlaceholdersAfterRemove');
         this.presenter.eventBus = {sendEvent: stubs.sendEvent};
+        this.presenter.$view.find('div[draggableitem="200_2"]').addClass('dragging');
 
         this.presenter.itemStopped(placeholder);
 
         assertTrue(stubs.sendEvent.calledTwice);
         assertEquals('ValueChanged', stubs.sendEvent.getCall(0).args[0]);
         assertEquals('itemStopped', stubs.sendEvent.getCall(1).args[0]);
+    },
+
+    'test dragging class added on helper creation': function() {
+        var placeholder = $(this.presenter.$view.find('.placeholder')[0]);
+        var stubs = {};
+
+        stubs.sendEvent = sinon.spy();
+        stubs.movePlaceholdersAfterRemove = sinon.stub( this.presenter, 'movePlaceholdersAfterRemove');
+        this.presenter.eventBus = {sendEvent: stubs.sendEvent};
+
+        assertFalse(placeholder.hasClass('dragging'));
+        placeholder.draggable('option', 'helper').call(placeholder);
+        assertTrue(placeholder.hasClass('dragging'));
     }
 });
