7a156c8ad090b326fbbde4f7b6f0684e9e059b33
WitoldDziurdz <witolddzyurdz@gmail.com>
2018-10-24 08:35:22 +0200

    iFrame visible in the editor when the hide option is enabled (#209)
    * iFrame visible in the editor when the hide option is enabled
    
    * add changelog
    
    * add test
    
    * poprawki po testach klienta

--- addons/Iframe/test/StateTests.js
+++ addons/Iframe/test/StateTests.js
@@ -3,25 +3,59 @@ TestCase("[Iframe] setState", {
         this.presenter = AddonIframe_create();
         this.validState = JSON.stringify({
             iframeState : "someState",
-            iframeScore : "SomeScore"
+            iframeScore : "SomeScore",
+            isVisible : true
         });
+        this.validState2 = JSON.stringify({
+            iframeState : "someState",
+            iframeScore : "SomeScore",
+            isVisible : false
+        });
+
+        this.validState3 = JSON.stringify({
+            iframeState : "someState",
+            iframeScore : "SomeScore",
+            isVisible : undefined
+        });
+
         this.undefinedState = JSON.stringify({
             iframeScore : "SomeScore"
         });
+        this.presenter.setVisibility = sinon.stub();
     },
 
-    'test state and score should be set as provided from player' : function () {
+    'test state and score and isVisible is true should be set as provided from player' : function () {
         var expectedIFrameState = "someState";
         var expectedIFrameScore = "SomeScore";
+        var expectedVisible = true;
         this.presenter.setState(this.validState);
 
         assertEquals(this.presenter.iframeState, expectedIFrameState);
         assertEquals(this.presenter.iframeScore, expectedIFrameScore);
+        assertEquals(this.presenter.isVisible, expectedVisible);
     },
 
-    'test when state is empty String should be undefined iframeState' : function () {
+    'test state and score and isVisible is false should be set as provided from player' : function () {
+        var expectedIFrameState = "someState";
+        var expectedIFrameScore = "SomeScore";
+        var expectedVisible = false;
+        this.presenter.setState(this.validState2);
+
+        assertEquals(this.presenter.iframeState, expectedIFrameState);
+        assertEquals(this.presenter.iframeScore, expectedIFrameScore);
+        assertEquals(this.presenter.isVisible, expectedVisible);
+    },
+
+    'test when state.isVisible is undefined pesenter.isVisible should be true' : function () {
+         var expectedVisible = true;
+        this.presenter.setState(this.validState3);
+
+        assertEquals(this.presenter.isVisible, expectedVisible);
+    },
+
+    'test when state. is empty String should be undefined iframeState' : function () {
         var expectedIFrameState = undefined;
-        this.presenter.setState(this.undefinedScore);
+        this.presenter.setState(this.undefinedState);
 
         assertEquals(this.presenter.iframeState, expectedIFrameState);
     }
@@ -34,9 +68,17 @@ TestCase("[Iframe] getState", {
         this.presenter.iframeScore = "someOtherScore";
     },
 
-    'test state shoould be the same as from Iframe' : function () {
+    'test when state.isVisible is true presenter shoould be the same as from Iframe' : function () {
+        this.presenter.isVisible = true;
+        var expectedState = JSON.stringify({iframeState: "SomeState", iframeScore: "someOtherScore", isVisible:true});
 
-        var expectedState = JSON.stringify({iframeState: "SomeState", iframeScore: "someOtherScore"});
+        var result = this.presenter.getState();
+        assertEquals(result, expectedState);
+    },
+
+    'test when state.isVisible is false presenter shoould be the same as from Iframe' : function () {
+        this.presenter.isVisible = false;
+        var expectedState = JSON.stringify({iframeState: "SomeState", iframeScore: "someOtherScore", isVisible:false});
 
         var result = this.presenter.getState();
         assertEquals(result, expectedState);
