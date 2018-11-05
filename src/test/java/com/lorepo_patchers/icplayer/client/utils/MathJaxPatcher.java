d680e477e4d67accb34cdd1152026e5a851c3414
tkrassowski <tomasz.krassowski@solwit.com>
2018-11-05 10:45:40 +0100

    [MathJax][Tests] Fix for MathJax patcher, re #6885 (#229)

--- src/test/java/com/lorepo_patchers/icplayer/client/utils/MathJaxPatcher.java
+++ src/test/java/com/lorepo_patchers/icplayer/client/utils/MathJaxPatcher.java
@@ -1,5 +1,6 @@
 package com.lorepo_patchers.icplayer.client.utils;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
 import com.googlecode.gwt.test.patchers.PatchClass;
 import com.googlecode.gwt.test.patchers.PatchMethod;
@@ -9,13 +10,14 @@ import com.lorepo.icplayer.client.utils.MathJaxElement;
 @PatchClass(MathJax.class)
 public class MathJaxPatcher {
 	@PatchMethod
-	public static void refreshMathJax(Element e) {
-		//Nothing
-	}
+	public static void refreshMathJax(Element e) { }
 	
 	
 	@PatchMethod
-	public static void setCallbackForMathJaxLoaded(MathJaxElement element) {
-		//Nothing
+	public static JavaScriptObject setCallbackForMathJaxLoaded(MathJaxElement element) {
+		return JavaScriptObject.createObject();
 	}
+	
+	@PatchMethod
+	public static void removeMessageHookCallback(JavaScriptObject hook) { }
 }
