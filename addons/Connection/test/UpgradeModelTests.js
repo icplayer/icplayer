2cd25c8631d7e6154197a48447b85554756371d8
Adam Pietrzak <adam.pietrzak@solwit.com>
2019-01-24 08:21:10 +0100

    poprawienie nazw testow

--- addons/Connection/test/UpgradeModelTests.js
+++ addons/Connection/test/UpgradeModelTests.js
@@ -1,93 +1,93 @@
-UpgradeModelTests = TestCase("[Connection] Upgrade tasks tests");
+TestCase("[Connection] Upgrade tasks tests", {
+    setUp: function () {
+        this.presenter = AddonConnection_create();
 
-UpgradeModelTests.prototype.setUp = function() {
-    this.presenter = AddonConnection_create();
+        this.model = {
+            "Left column": [{
+                id: "",
+                content: "",
+                "connects to": "",
+                "additional class": ""
+            }],
+            "Right column": [{
+                id: "",
+                content: "",
+                "connects to": "",
+                "additional class": ""
+            }],
+            "Default connection color": "",
+            "Correct connection color": "",
+            "Incorrect connection color": "",
+            "Connection thickness, string": ""
+        };
 
-    this.model = {
-        "Left column": [{
-            id: "",
-            content: "",
-            "connects to": "",
-            "additional class": ""
-        }],
-        "Right column": [{
-            id: "",
-            content: "",
-            "connects to": "",
-            "additional class": ""
-        }],
-        "Default connection color": "",
-        "Correct connection color": "",
-        "Incorrect connection color": "",
-        "Connection thickness, string": ""
-    };
+        this.currentModel = {
+            "Left column": [{
+                id: "",
+                content: "",
+                "connects to": "",
+                "additional class": ""
+            }],
+            "Right column": [{
+                id: "",
+                content: "",
+                "connects to": "",
+                "additional class": ""
+            }],
+            "Columns width": [{
+                left: "",
+                middle: "",
+                right: ""
+            }],
+            "Default connection color": "",
+            "Correct connection color": "",
+            "Incorrect connection color": "",
+            "Connection thickness, string": "",
+            "disabledConnectionColor": "",
+            "initialConnections": []
+        };
+    },
 
-    this.currentModel = {
-        "Left column": [{
-            id: "",
-            content: "",
-            "connects to": "",
-            "additional class": ""
-        }],
-        "Right column": [{
-            id: "",
-            content: "",
-            "connects to": "",
-            "additional class": ""
-        }],
-        "Columns width": [{
+    'test given simple model when upgradeFromV_01 is called then will return upgraded model': function () {
+        var expectedColumnsWidth = [{
             left: "",
             middle: "",
             right: ""
-        }],
-        "Default connection color": "",
-        "Correct connection color": "",
-        "Incorrect connection color": "",
-        "Connection thickness, string": "",
-        "disabledConnectionColor": "",
-        "initialConnections": []
-    };
-};
+        }];
 
-UpgradeModelTests.prototype.testFromV_01 = function() {
-    var expectedColumnsWidth = [{
-        left: "",
-        middle: "",
-        right: ""
-    }];
+        var upgradedModel = this.presenter.upgradeFrom_01(this.model);
 
-    var upgradedModel = this.presenter.upgradeFrom_01(this.model);
+        assertEquals(expectedColumnsWidth, upgradedModel["Columns width"]);
+        assertNotEquals(this.model, upgradedModel); // Ensure that changes were made on copy
+    },
 
-    assertEquals(expectedColumnsWidth, upgradedModel["Columns width"]);
-    assertNotEquals(this.model, upgradedModel); // Ensure that changes were made on copy
-};
+    'test given simple model when upgradeModel is called then will return newest version of model': function () {
+        var upgradedModel = this.presenter.upgradeModel(this.model);
 
-UpgradeModelTests.prototype.testUpgradeToCurrentVersion = function() {
-    var upgradedModel = this.presenter.upgradeModel(this.model);
+        assertEquals(this.currentModel, upgradedModel);
+        assertNotEquals(this.model, upgradedModel);
+    },
 
-    assertEquals(this.currentModel, upgradedModel);
-    assertNotEquals(this.model, upgradedModel);
-};
+    'test given empty model when upgrade start values is called then returns upgraded model': function () {
+        var emptyModel = {};
+        var expectedModel = {
+            "disabledConnectionColor": "",
+            "initialConnections": []
+        };
+        var upgradedModel = this.presenter.upgradeStartValues(emptyModel);
 
-UpgradeModelTests.prototype.testGivenEmptyModelWhenUpgradeStartValuesIsCalledThenReturnsUpgradedModel = function () {
-    var emptyModel = {};
-    var expectedModel = {
-        "disabledConnectionColor": "",
-        "initialConnections": []
-    };
-    var upgradedModel = this.presenter.upgradeStartValues(emptyModel);
+        assertNotEquals(emptyModel, upgradedModel);
+        assertEquals(expectedModel, upgradedModel);
+    },
 
-    assertNotEquals(emptyModel, upgradedModel);
-    assertEquals(expectedModel, upgradedModel);
-};
+    'test given model with filled disabled fields when upgrade model is called then will return the same object': function () {
+        var modelToUpgrade = {
+            "disabledConnectionColor": "abc",
+            "initialConnections": ["a", "b", "C"]
+        };
 
-UpgradeModelTests.prototype.testGivenModelWithFilledDisabledFieldsWhenUpgradeModelIsCalledThenWillReturnTheSameObject = function () {
-    var modelToUpgrade = {
-        "disabledConnectionColor": "abc",
-        "initialConnections": ["a", "b", "C"]
-    };
+        var upgradedModel = this.presenter.upgradeStartValues(modelToUpgrade);
 
-    var upgradedModel = this.presenter.upgradeStartValues(modelToUpgrade);
-
-    assertEquals(modelToUpgrade, upgradedModel);
-};
+        assertEquals(modelToUpgrade, upgradedModel);
+    }
+});
