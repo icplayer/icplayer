087345211adddd92c0aa5729a17840a7d3e43b7f
Wojciech Dobkowicz <37017975+wdobkowicz@users.noreply.github.com>
2019-08-06 09:30:44 +0200

    New Addon FlashCards, re #7095 (#318)
    * Add files via upload
    
    * Fixed Flash Card not loading properly
    
    * Update presenter.js
    
    * Update presenter.js
    
    * Update presenter.js
    
    * Update FlashCards info
    
    * Update presenter.js
    
    * Update presenter.js

--- src/main/java/com/lorepo/icplayer/public/libs/dictionaries/bl/dictionary.js
+++ src/main/java/com/lorepo/icplayer/public/libs/dictionaries/bl/dictionary.js
@@ -1977,5 +1977,7 @@ var ice_dictionary_bl = {
 	"EditableWindow_property_video_file": "Видео файл",
 	"EditableWindow_property_video_title": "Заглавие",
 	"EditableWindow_property_header_style": "Стил на горния колонтитул",
-	"EditableWindow_property_editing_enabled" : "Разреши редактиране"
+	"EditableWindow_property_editing_enabled" : "Разреши редактиране",
+	"FlashCards_name" : "Flash Cards",
+    "FlashCards_info" : "In Flash Cards module you can create cards with varied content such as text, images and audio, which are different at the front and back of the card. Each card can be marked as favourite and graded by the student as correct or wrong in the activity mode."
 };
