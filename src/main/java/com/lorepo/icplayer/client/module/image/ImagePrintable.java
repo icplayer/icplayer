4657b25b77610a38a4f5cda84e995acfc2309163
Maciej Zawłocki <mzawlocki@solwit.com>
2021-01-20 10:23:44 +0100

    further fixes

--- src/main/java/com/lorepo/icplayer/client/module/image/ImagePrintable.java
+++ src/main/java/com/lorepo/icplayer/client/module/image/ImagePrintable.java
@@ -1,6 +1,5 @@
 package com.lorepo.icplayer.client.module.image;
 
-import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.Image;
 import com.lorepo.icplayer.client.module.image.ImageModule.DisplayMode;
 import com.lorepo.icplayer.client.printable.PrintableContentParser;
@@ -13,13 +12,6 @@ public class ImagePrintable {
 	public ImagePrintable(ImageModule model) {
 		this.model = model;
 	}
-	
-	private void keepAspect(Image image, int width, int height) {
-		ImageViewUtils.keepAspect(image, model.getWidth(), model.getHeight());
-		Style style = image.getElement().getStyle();
-		style.setPosition(Style.Position.ABSOLUTE);
-		style.setLeft((width - image.getWidth())/2, Style.Unit.PX);
-	}
 
 	public String getPrintableHTML(String className, boolean showAnswers) {
 		if (model.getPrintable() == PrintableMode.NO) return null;
@@ -27,15 +19,16 @@ public class ImagePrintable {
 		String rootStyle = "width:"+Integer.toString(model.getWidth())+"px;";
 		rootStyle += "height:"+Integer.toString(model.getHeight())+"px;";
 		rootStyle += "position: relative;";
-		String result = "<div class=\"printable_ic_image\" id=\"" + model.getId() + "\" style=\"" + rootStyle + "\">";
+		String rootClass = "printable_ic_image";
+		if (model.getDisplayMode() == DisplayMode.keepAspect) {
+			rootClass += " keepAspect";
+		}
+		String result = "<div class=\"" + rootClass + "\" id=\"" + model.getId() + "\" style=\"" + rootStyle + "\">";
 		
-		Image image = new Image();
+		final Image image = new Image();
 		image.setUrl(model.getUrl());
 		if(model.getDisplayMode() == DisplayMode.stretch){
 			image.setPixelSize(model.getWidth(), model.getHeight());
-		}
-		else if(model.getDisplayMode() == DisplayMode.keepAspect){
-			keepAspect(image, model.getWidth(), model.getHeight());
 		}
 		else if(model.getDisplayMode() == DisplayMode.originalSize){
 			image.setVisibleRect(0, 0, model.getWidth(), model.getHeight());
