20af15eaff86ecc9a4ed3ad10d1edea817672858
Michał Grzeliński <michal.grzelinski@solwit.com>
2021-03-29 11:54:35 +0200

    Implementation of audio files export

--- src/main/java/com/lorepo/icplayer/client/content/services/AssetsService.java
+++ src/main/java/com/lorepo/icplayer/client/content/services/AssetsService.java
@@ -1,11 +1,16 @@
 package com.lorepo.icplayer.client.content.services;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 import com.lorepo.icplayer.client.model.IAsset;
 import com.lorepo.icplayer.client.module.api.player.IAssetsService;
 import com.lorepo.icplayer.client.module.api.player.IContent;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.lorepo.icf.utils.JavaScriptUtils;
 
-public class AssetsService implements IAssetsService{
+public class AssetsService implements IAssetsService {
 	
 	private final List<IAsset> assets;
 	
@@ -25,5 +30,24 @@ public class AssetsService implements IAssetsService{
 		}
 
 		return contentType;
+	}
+
+	@Override
+	public List<JavaScriptObject> getAssetsAsJS() {
+		List<JavaScriptObject> assetsList = new ArrayList<JavaScriptObject>();
+		for (IAsset asset : assets) {
+			assetsList.add(this.mapFromAsset(asset));
+		}
+		return assetsList;
+	}
+
+	public JavaScriptObject mapFromAsset(IAsset asset) {
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		map.put("fileName", asset.getFileName());
+		map.put("type", asset.getContentType());
+		map.put("href", asset.getHref());
+
+		return JavaScriptUtils.createHashMap(map);
 	}
 }
