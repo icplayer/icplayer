8a94e52e01c88edf13bb4635d2254c27353055b8
msawoniuk <79645404+msawoniuk@users.noreply.github.com>
2021-05-27 11:13:38 +0200

    Add support for printable state in ordering, re #7908 (#533)
    * added support for setState in printable
    
    * Added support for printable state in Ordering addon, re #7908
    
    * Add printable representation for state 4
    
    Add printable support for alternative correct order
    
    * Update changelog.txt
    
    * Add information in changelog
    
    * Resolve issues after Code Review
    
    * Refactor code to use DOM elements
    
    Commit also contains:
    * Add tests to functionality
    
    * Rename classes to match the addon they support
    
    * Add different class for show-answers mode. Check if user had interaction. Add alt text parses
    
    * Fix issues. Fix tests
    
    * Add proper classes and behaviour when user hasnt worked with module
    
    Co-authored-by: Maciej Zawłocki <mzawlocki@solwit.com>
    Co-authored-by: Tomasz Krassowski <tkrassowski@LAP171.local>

--- src/main/java/com/lorepo/icplayer/client/module/ordering/OrderingPrintable.java
+++ src/main/java/com/lorepo/icplayer/client/module/ordering/OrderingPrintable.java
@@ -1,93 +1,205 @@
 package com.lorepo.icplayer.client.module.ordering;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Map;
 
-import com.google.gwt.user.client.Random;
-import com.lorepo.icf.utils.JavaScriptUtils;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.DOM;
 import com.lorepo.icplayer.client.printable.PrintableContentParser;
 import com.lorepo.icplayer.client.printable.PrintableController;
 
 public class OrderingPrintable {
 
-	OrderingModule model = null;
-	PrintableController controller = null;
-	
-	public OrderingPrintable(OrderingModule model, PrintableController controller) {
-		this.model = model;
-		this.controller = controller;
-	}
-	
-	public String getPrintableHTML(String className, boolean showAnswers) {
-		String result = "<div class=\"printable_ic_ordering\" id=\"" + model.getId() +"\">";
-		
-		String[] parsedItems = new String[model.getItemCount()];
-		List<String> unorderedItems = new ArrayList<String>();
-		
-		for (int i = 0; i < model.getItemCount(); i++) {
-			OrderingItem item = model.getItem(i);
-			String parsedItem = "";
-			if (showAnswers) {
-				parsedItem = createItem(item, model.isVertical(), i+1);
-			} else {
-				parsedItem = createItem(item, model.isVertical(), 0);
-			}
-			Integer startingPosition = item.getStartingPosition();
-			if (startingPosition != null) {
-				parsedItems[startingPosition - 1] = parsedItem;
-			} else {
-				unorderedItems.add(parsedItem);
-			}
-		}
-		
-		for(int index = 0; index < unorderedItems.size(); index += 1) {  
-			Collections.swap(unorderedItems, index, index + nextInt(unorderedItems.size() - index));  
-		}
-		
-		for (int i = 0; i < parsedItems.length; i++) {
-			if (parsedItems[i] == null) {
-				parsedItems[i] = unorderedItems.get(0);
-				unorderedItems.remove(0);
-			}
-		}
-		
-		for (int i = 0; i < parsedItems.length; i++) {
-			result += parsedItems[i];
-		}
-		
-		result += "</div>";
-		result = PrintableContentParser.addClassToPrintableModule(result, className, !model.isSplitInPrintBlocked());
-		return result;
-	}
-	
-	private String createItem(OrderingItem item, boolean isVertical, int index) {
-		// index == 0 means no index will be displayed
-		String result = "";
-		String displayMode = isVertical? "block" : "inline-block";
-		result += "<div style=\"display:" + displayMode + "\">";
-		result += "<div class=\"item-wrapper\">";
-		result += "<table><tr><td>";
-		result += "<div class=\"number-box\">";
-		if (index != 0) {
-		    result += Integer.toString(index);
-		}
-		result += "</div>";
-		result += "</td><td>";
-		result += item.getText();
-		result += "</td></tr></table></div></div>";
-		return result;
-	}
-	
-	private int nextInt(int upperBound) {
-		int result = 0;
-		if (controller != null) {
-			result = controller.nextInt(upperBound);
-		} else {
-			result = Random.nextInt(upperBound);
-		}
-		return result;
-	}
-	
+    IPrintableOrderingModule model;
+    PrintableController controller;
+    OrderingPrintableCollection collection ;
+    Map<String, String> printableState = null;
+
+    private enum IndexBoxClass {
+        DEFAULT("number-box"),
+        CORRECT("number-box-correct"),
+        WRONG("number-box-wrong");
+
+        private final String className;
+
+        IndexBoxClass(String className) {
+            this.className = className;
+        }
+
+        public String className() {
+            return this.className;
+        }
+    }
+
+    public OrderingPrintable(IPrintableOrderingModule model, PrintableController controller) {
+        this.model = model;
+        this.controller = controller;
+        this.collection = new OrderingPrintableCollection(controller, model.getItemCount());
+    }
+
+    public String getPrintableHTML(String className, boolean showAnswers) {
+        if (!this.model.isPrintable()) {
+            return null;
+        }
+
+        printableState = model.getPrintableState();
+        boolean stateSet = printableState != null;
+
+        boolean isPrintableState = stateSet && printableState.containsKey("order");
+        boolean userHasInteracted = stateSet && printableState.containsKey("isSolved") && printableState.get("isSolved").toLowerCase().equals("true");
+
+        Element representation;
+
+        if (isPrintableState && userHasInteracted) {
+            // states CHECK_ANSWERS (4) (showAnswers = true) or SHOW_USER_ANSWERS (3) (showAnswers = false)
+            representation = createItemsWithUserAnswers(showAnswers);
+        } else if (isPrintableState) {
+            // states CHECK_ANSWERS (4) or SHOW_USER_ANSWERS (3) when user hasn't answered - show empty boxes
+            representation = createItems(false);
+        } else {
+            // states SHOW_ANSWERS (2) (showAnswers = true) or EMPTY (1) (showAnswers = false)
+            representation = createItems(showAnswers);
+
+            if (showAnswers) {
+                representation.addClassName("printable-ordering-show-answers");
+            }
+        }
+
+        return PrintableContentParser.addClassToPrintableModule(representation.getString(), className, !this.model.isSplitInPrintBlocked());
+    }
+
+    private Element createItemsWithUserAnswers(boolean showCorrectness) {
+        Integer[] itemsIndexesFromState = getItemsIndexesInOrderFromState(this.printableState);
+        boolean isAllInCorrectOrder = true;
+
+        for (int i = 0; i < itemsIndexesFromState.length; i++) {
+            int itemPosition = i + 1;
+            for (int j = 0; j < model.getItemCount(); j++) {
+                OrderingItem item = model.getItem(j);
+                if (item.getIndex() == itemsIndexesFromState[i]) {
+                    boolean isCorrectOrder = item.isCorrect(itemPosition);
+                    isAllInCorrectOrder &= isCorrectOrder;
+
+                    String indexBoxClassName = IndexBoxClass.DEFAULT.className();
+                    if (showCorrectness) {
+                        indexBoxClassName = isCorrectOrder ? IndexBoxClass.CORRECT.className() : IndexBoxClass.WRONG.className();
+                    }
+
+                    Element indexBox = createPrintableIndexBox(itemPosition, indexBoxClassName);
+                    Element printableItem = createPrintableItem(item, indexBox);
+                    collection.addItemToItems(item, printableItem);
+                    break;
+                }
+            }
+        }
+        collection.sortAndMergeItems();
+
+        Element classWrapper = createPrintableClassWrapper();
+        Element items = combinePrintableItems(collection.getOrderedItems());
+        classWrapper.appendChild(items);
+
+        if (showCorrectness) {
+            Element sign = createSign(isAllInCorrectOrder);
+            classWrapper.appendChild(sign);
+        }
+
+        return classWrapper;
+    }
+
+    private Element createItems(boolean showAnswers) {
+        OrderingItem item;
+
+        for (int i = 0; i < model.getItemCount(); i++) {
+            item = model.getItem(i);
+
+            Element indexBox = showAnswers ?
+                    createPrintableIndexBox(item.getIndex(), IndexBoxClass.DEFAULT.className()) :
+                    createPrintableEmptyIndexBox();
+
+            Element printableItem = createPrintableItem(item, indexBox);
+
+            collection.addItemToItems(item, printableItem);
+        }
+        collection.sortAndMergeItems();
+
+        Element classWrapper = createPrintableClassWrapper();
+        Element items = combinePrintableItems(collection.getOrderedItems());
+        classWrapper.appendChild(items);
+        return classWrapper;
+    }
+
+    /**
+     * getItemsIndexesInOrderFromState Returns items indexes in order defined in the printable state.
+     *
+     * @param printableState printable state object in which there must be a 'order' key. Resulting value must be of
+     *                       String type. Numbers represent indexes of items. The numbers are arranged in order.
+     *                       Numbers must be separated by a comma. Example: "3,1,2"
+     * @return indexes items in order from the state
+     */
+    private Integer[] getItemsIndexesInOrderFromState(Map<String, String> printableState) {
+        String orderState = printableState.get("order");
+        String[] indexes = orderState.split(",");
+        Integer[] parsedIndexes = new Integer[indexes.length];
+        for (int i = 0; i < indexes.length; i++) {
+            parsedIndexes[i] = Integer.parseInt(indexes[i]);
+        }
+        return parsedIndexes;
+    }
+
+    private Element createIndexBox(String innerHTML) {
+        Element box = DOM.createDiv();
+        box.setInnerHTML(innerHTML);
+        return box;
+    }
+
+    private Element createPrintableEmptyIndexBox() {
+        Element box = DOM.createDiv();
+        box.setClassName("number-box");
+        return box;
+    }
+
+    private Element createPrintableIndexBox(Integer index, String className) {
+        Element box = createIndexBox(index.toString());
+        box.setClassName(className);
+        return box;
+    }
+
+    private Element createPrintableItem(OrderingItem item, Element indexBox) {
+        Element printableItem = DOM.createDiv();
+
+        Style style = printableItem.getStyle();
+        Style.Display displayType = this.model.isVertical() ? Style.Display.BLOCK : Style.Display.INLINE_BLOCK;
+        style.setDisplay(displayType);
+
+        Element childHTML = item.toPrintableDOMElement(indexBox);
+        printableItem.appendChild(childHTML);
+
+        return printableItem;
+    }
+
+    private Element combinePrintableItems(Element[] items) {
+        Element div = DOM.createDiv();
+        div.setClassName("items-wrapper");
+        for (Element printableItem : items) {
+            div.appendChild(printableItem);
+        }
+        return div;
+    }
+
+    private Element createSign(boolean isAllCorrect) {
+        Element sign = DOM.createDiv();
+
+        String className = isAllCorrect ? "is-all-ok-sign-wrapper" : "is-not-all-ok-sign-wrapper";
+        sign.setClassName(className);
+
+        return sign;
+    }
+
+    private Element createPrintableClassWrapper() {
+        Element wrapper = DOM.createDiv();
+        wrapper.setClassName("printable_ic_ordering");
+        wrapper.setId(this.model.getId());
+        return wrapper;
+    }
+
 }
---
+++ src/test/java/com/lorepo/icplayer/client/module/ordering/GWTOrderingPrintableCollectionTestCase.java
@@ -0,0 +1,125 @@
+package com.lorepo.icplayer.client.module.ordering;
+
+import static org.junit.Assert.*;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
+import com.lorepo.icplayer.client.GWTPowerMockitoTest;
+import com.lorepo.icplayer.client.printable.PrintableController;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+
+public class GWTOrderingPrintableCollectionTestCase extends GWTPowerMockitoTest {
+	PrintableController printableController = null;
+	OrderingModule model = null;
+
+	Integer itemsAmount = 3;
+	OrderingItem[] items = new OrderingItem[itemsAmount];
+	OrderingItem item1 = null;
+	OrderingItem item2 = null;
+	OrderingItem item3 = null;
+
+	Element[] itemsReps = new Element[itemsAmount];
+	Element item1Rep = null;
+	Element item2Rep = null;
+	Element item3Rep = null;
+
+
+	@Before
+	public void setUp() throws Exception {
+		this.model = Mockito.mock(OrderingModule.class);
+		this.printableController = Mockito.mock(PrintableController.class);
+		Mockito.doReturn(itemsAmount).when(this.model).getItemCount();
+
+		item1Rep = DOM.createDiv();
+		item1Rep.setId("1");
+		itemsReps[0] = item1Rep;
+
+		item2Rep = DOM.createDiv();
+		item2Rep.setId("2");
+		itemsReps[1] = item2Rep;
+
+		item3Rep = DOM.createDiv();
+		item3Rep.setId("3");
+		itemsReps[2] = item3Rep;
+	}
+
+	@Test
+	public void addItemToAppropriateArrayWhenAllItemsHaveStartingPosition() {
+		item1 = new OrderingItem(1, "element 1", "string", 1);
+		item2 = new OrderingItem(2, "element 2", "string", 2);
+		item3 = new OrderingItem(3, "element 3", "string", 3);
+		addItems();
+
+		int tries = 8;
+		for (int tryID = 0; tryID < tries; tryID++) {
+			OrderingPrintableCollection collection = new OrderingPrintableCollection(
+					this.printableController, this.model.getItemCount());
+			executeAddItemToAppropriateArrayForAllItems(collection);
+			Element[] items = collection.getOrderedItems();
+			for (int i = 0; i < items.length; i++) {
+				Element item = items[i];
+				assertEquals(item.getId(), itemsReps[i].getId());
+			}
+		}
+	}
+
+	@Test
+	public void addItemToAppropriateArrayWhenAllItemsDoNotHaveStartingPosition() {
+		item1 = new OrderingItem(1, "element 1", "string", null);
+		item2 = new OrderingItem(2, "element 2", "string", null);
+		item3 = new OrderingItem(3, "element 3", "string", 2);
+		addItems();
+
+		int tries = 8;
+		for (int tryID = 0; tryID < tries; tryID++) {
+			OrderingPrintableCollection collection = new OrderingPrintableCollection(
+					this.printableController, this.model.getItemCount());
+			executeAddItemToAppropriateArrayForAllItems(collection);
+			Element[] items = collection.getOrderedItems();
+			assertNull(items[0]);
+			assertEquals(items[1], this.item3Rep);
+			assertNull(items[2]);
+		}
+	}
+
+	@Test
+	public void sortAndMergeItemsWithController() {
+		item1 = new OrderingItem(1, "element 1", "string", null);
+		item2 = new OrderingItem(2, "element 2", "string", null);
+		item3 = new OrderingItem(3, "element 3", "string", 2);
+		addItems();
+
+		int tries = 8;
+		for (int tryID = 0; tryID < tries; tryID++) {
+			OrderingPrintableCollection collection = new OrderingPrintableCollection(
+					this.printableController, this.model.getItemCount());
+			executeAddItemToAppropriateArrayForAllItems(collection);
+
+			Mockito.when(this.printableController.nextInt(Mockito.isA(int.class))).thenReturn(3, 1);
+			collection.sortAndMergeItems();
+
+			Element[] items = collection.getOrderedItems();
+			assertEquals(items[0], this.item1Rep);
+			assertEquals(items[1], this.item3Rep);
+			assertEquals(items[2], this.item2Rep);
+		}
+	}
+
+	private void addItems(){
+		items[0] = item1;
+		items[1] = item2;
+		items[2] = item3;
+	}
+
+	private void executeAddItemToAppropriateArrayForAllItems(
+			OrderingPrintableCollection collection) {
+		for (int i = 0; i < itemsAmount; i++) {
+			collection.addItemToItems(items[i], itemsReps[i]);
+		}
+	}
+}
