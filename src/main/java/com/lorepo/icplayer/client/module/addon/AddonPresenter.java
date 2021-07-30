5945fc779d8a62d310e24ff6f9fc34e3b8a12152
Maciej Zawłocki <mzawlocki@solwit.com>
2021-07-30 10:12:17 +0200

    feat(addons): added getOpenEndedContentsForCurrentPage to PlayerServices

--- src/main/java/com/lorepo/icplayer/client/module/addon/AddonPresenter.java
+++ src/main/java/com/lorepo/icplayer/client/module/addon/AddonPresenter.java
@@ -29,6 +29,7 @@ import com.lorepo.icf.scripting.IType;
 import com.lorepo.icf.utils.StringUtils;
 import com.lorepo.icf.utils.URLUtils;
 import com.lorepo.icplayer.client.content.services.PlayerEventBusWrapper;
+import com.lorepo.icplayer.client.module.IOpenEndedContentPresenter;
 import com.lorepo.icplayer.client.module.IWCAG;
 import com.lorepo.icplayer.client.module.IWCAGModuleView;
 import com.lorepo.icplayer.client.module.IWCAGPresenter;
@@ -43,7 +44,7 @@ import com.lorepo.icplayer.client.page.KeyboardNavigationController;
 import com.lorepo.icplayer.client.page.PageController;
 
 
-public class AddonPresenter implements IPresenter, IActivity, IStateful, ICommandReceiver, IWCAGPresenter, IWCAG, IWCAGModuleView, IGradualShowAnswersPresenter {
+public class AddonPresenter implements IPresenter, IActivity, IStateful, ICommandReceiver, IWCAGPresenter, IWCAG, IWCAGModuleView, IGradualShowAnswersPresenter, IOpenEndedContentPresenter {
 
 	public interface IDisplay extends IModuleView{
 		public Element getElement();
@@ -623,6 +624,19 @@ public class AddonPresenter implements IPresenter, IActivity, IStateful, IComman
 	private native void setEventBus(JavaScriptObject presenter, JavaScriptObject eventBusWrapper) /*-{
 		if (presenter.setEventBus != undefined) {
 			presenter.setEventBus(eventBusWrapper);
+		}
+	}-*/;
+
+	@Override
+	public String getOpenEndedContent() {
+		return getOpenEndedContent(jsObject);
+	}
+
+	private native String getOpenEndedContent(JavaScriptObject presenter)/*-{
+		if (presenter && presenter.hasOwnProperty("getOpenEndedContent")) {
+			return presenter.getOpenEndedContent();
+		} else {
+			return null;
 		}
 	}-*/;
 }
