24a7dcef307fc596265a66b2a3d268d85c14cdb4
karol-f-solwit <75782376+karol-f-solwit@users.noreply.github.com>
2021-08-05 11:55:01 +0200

    fix(addon): fixing addon test ClickHandling, AssessmentsNavBar, Audio, GapsContainerObject re #8057 (#590)
    * fix(addon): fixing test ClickHandlingTests.js - 1. addon re #8011
    
    * fix(addon): Fix tests Assessments_Navigation_Bar addon re #8011
    
    * fix(addon): Audio addon fix test re #8011
    
    * fix(addon): fix test GapsContainerObjectTests re #8011
    
    * fix(addon): fixing addon test ClickHandling, AssessmentsNavBar, Audio, GapsContainerObject re #8057

--- addons/Assessments_Navigation_Bar/test/UtilsTests.js
+++ addons/Assessments_Navigation_Bar/test/UtilsTests.js
@@ -25,6 +25,12 @@ TestCase("[Assessments_Navigation_Bar] Change to Page", {
     setUp: function () {
         this.presenter = AddonAssessments_Navigation_Bar_create();
 
+        this.stubs = { getPagesMappingStub: sinon.stub() };
+
+        this.playerControllerStub = { getPagesMapping: this.stubs.getPagesMappingStub };
+
+        this.presenter.playerController = this.playerControllerStub;
+
         this.gotoPageStub = sinon.stub();
         this.presenter.currentPageIndex = 5;
 
@@ -35,12 +41,14 @@ TestCase("[Assessments_Navigation_Bar] Change to Page", {
     },
 
     'test shouldnt change page if provided index is equal to current page index': function () {
+        this.stubs.getPagesMappingStub.returns([0, 1, 2, 3]);
         this.presenter.changeToPage(5);
 
         assertFalse(this.gotoPageStub.called);
     },
 
     'test should change page if provided index is diffrent then current page index': function () {
+        this.stubs.getPagesMappingStub.returns([0, 1, 2, 3]);
         this.presenter.changeToPage(4);
 
         assertTrue(this.gotoPageStub.calledOnce);
