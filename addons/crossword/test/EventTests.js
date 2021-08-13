7b42fe9ca7b814d9f52fd2afbac4fc3aefb215b2
Jędrzej Niemiera <jniemiera@solwit.com>
2021-08-13 14:36:42 +0200

    Added more event tests

--- addons/crossword/test/EventTests.js
+++ addons/crossword/test/EventTests.js
@@ -2,12 +2,18 @@ TestCase("Events test", {
     setUp: function() {
         this.presenter = Addoncrossword_create();
         this.presenter.addonID = 'crossword1';
+        this.presenter.isGradualShowAnswersActive = false;
+
         sinon.stub(this.presenter, 'sendAllOKEvent');
         sinon.stub(this.presenter, 'isAllOK');
-        this.stubs = {
-            gradualShowAnswers: sinon.stub()
-        };
-        this.presenter.gradualShowAnswers = this.stubs.gradualShowAnswers;
+
+        this.spies = {
+            showAnswers: sinon.spy(this.presenter, 'showAnswers'),
+            hideAnswers: sinon.spy(this.presenter, 'hideAnswers'),
+            gradualShowAnswers: sinon.spy(this.presenter, 'gradualShowAnswers')
+        }
+
+
     },
 
     tearDown: function() {
@@ -29,13 +35,40 @@ TestCase("Events test", {
         assertFalse(this.presenter.sendAllOKEvent.called);
     },
 
-    'test GSA event calls the right method': function () {
+    'test showAnswers event calls the right method': function () {
+        var eventName = "ShowAnswers";
+        var eventData = {};
+        this.presenter.onEventReceived(eventName, eventData);
+
+        assertTrue(this.spies.showAnswers.called);
+    },
+
+    'test hideAnswers event calls the right method': function () {
+        var eventName = "HideAnswers";
+        var eventData = {};
+        this.presenter.onEventReceived(eventName, eventData);
+
+        assertTrue(this.spies.hideAnswers.called);
+    },
+
+    'test GSA event calls the right method and changes isGradualShowAnswersActive to true': function () {
         var eventName = "GradualShowAnswers";
         var eventData = {
             moduleID: 'crossword1'
         };
         this.presenter.onEventReceived(eventName, eventData);
 
-        assertEquals(1, this.stubs.gradualShowAnswers.callCount);
+        assertTrue(this.spies.gradualShowAnswers.called);
+        assertTrue(this.presenter.isGradualShowAnswersActive);
+    },
+
+    'test GHA event calls the right method and changes isGradualShowAnswersActive to false': function () {
+        this.presenter.isGradualShowAnswersActive = true;
+        var eventName = "GradualHideAnswers";
+        var eventData = {};
+        this.presenter.onEventReceived(eventName, eventData);
+
+        assertTrue(this.spies.hideAnswers.called);
+        assertFalse(this.presenter.isGradualShowAnswersActive);
     }
 });
