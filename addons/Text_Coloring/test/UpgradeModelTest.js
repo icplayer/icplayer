26689fe30685344a4aaa2b379ff216e5f32bac39
wkarasSolwit <wojciech.karas@solwit.com>
2021-09-03 14:00:45 +0200

    test(addons) Text Coloring correcting tests after mering with master re #7914

--- addons/Text_Coloring/test/UpgradeModelTest.js
+++ addons/Text_Coloring/test/UpgradeModelTest.js
@@ -6,29 +6,14 @@ TestCase("[Text_Coloring] Upgrade Text Coloring model", {
             Bottom: '',
             ID: 'Text_Coloring1'
         };
-
-        this.addModelPropertySpy = sinon.spy(this.presenter, 'addModelProperty');
     },
 
     'test should add properties to model when the upgradeModel was called': function () {
         var upgradedModel = this.presenter.upgradeModel(this.model);
 
-        assertTrue(this.addModelPropertySpy.called);
         assertTrue(upgradedModel.hasOwnProperty('Mode'));
         assertTrue(upgradedModel.hasOwnProperty('countErrors'));
-        assertTrue(upgradedModel.hasOwnProperty('printable'));
+        assertTrue(upgradedModel.hasOwnProperty('showAllAnswersInGradualShowAnswersMode'));
         assertTrue(upgradedModel.hasOwnProperty('Legend title'));
-    },
-
-    'test given new property when addModelProperty was called should add the property to the model': function () {
-        var property = {
-            name: 'Property test',
-            value: 'Property test value'
-        };
-
-        var upgradedModel = this.presenter.addModelProperty(this.model, property.name, property.value);
-
-        assertTrue(upgradedModel.hasOwnProperty('Property test'));
-        assertEquals(upgradedModel['Property test'], 'Property test value');
     }
 });
