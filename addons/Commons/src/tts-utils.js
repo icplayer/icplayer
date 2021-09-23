73bbe3c745bbb6dce22addfb809b6834c52ef9f4
Radosław Wojdak <rwojdak@solwit.com>
2021-09-23 07:59:23 +0200

    Move converting code to tts-utils and fix language bugs #8129

--- addons/Commons/src/tts-utils.js
+++ addons/Commons/src/tts-utils.js
@@ -11,6 +11,12 @@
             empty: '&&empty&&'
         },
 
+        const GENDER = {
+            MASCULINE: 0,
+            FEMININE: 1,
+            NEUTER: 2,
+        },
+
         getTextVoiceArrayFromElement: function($element, presenterLangTag) {
             var $clone = $element.clone();
 
@@ -115,6 +121,49 @@
             text = text.replace(/\\alt{([^{}|]*?)\|[^{}|]*?}(\[[a-zA-Z0-9_\- ]*?\])*/g, '$1'); // replace \alt{a|b}[c] with a
             text = text.replace(/\\alt{([^|{}]*?)\|[^|{}]*?}/g, '$1'); // replace \alt{a|b} with a
             return text;
+        },
+
+        numberToPolishNumber: function(n) {
+            if (n === 0)
+                return 'zero';
+
+            var textUnits = ['', 'jeden', 'dwa', 'trzy', 'cztery', 'pi\u0119\u0107', 'sze\u015b\u0107', 'siedem', 'osiem', 'dziewi\u0119\u0107'];
+            var textTeens = ['', 'jedena\u015bcie', 'dwana\u015bcie', 'trzyna\u015bcie', 'czterna\u015bcie', 'pi\u0119tna\u015bcie', 'szesna\u015bcie', 'siedemna\u015bcie', 'osiemna\u015bcie', 'dziewi\u0119tna\u015bcie'];
+            var textTens = ['', 'dziesi\u0119\u0107', 'dwadzie\u015bcia', 'trzydzie\u015bci', 'czterdzie\u015bci', 'pi\u0119\u0107dziesi\u0105t', 'sze\u015b\u0107dziesi\u0105t', 'siedemdziesi\u0105t', 'osiemdziesi\u0105t', 'dziewi\u0119\u0107dziesi\u0105t'];
+            var textHundreds = ['', 'sto', 'dwie\u015bcie', 'trzysta', 'czterysta', 'pi\u0119\u0107set', 'sze\u015b\u0107set', 'siedemset', 'osiemset', 'dziewi\u0119\u0107set'];
+
+            var units = Math.floor(n % 10);
+            var tens = Math.floor((n / 10) % 10);
+            var hundreds = Math.floor((n / 100) % 10);
+            var thousands = Math.floor((n % Math.pow(10, 6)) / Math.pow(10, 3));
+            var millions = Math.floor((n % Math.pow(10, 9)) / Math.pow(10, 6));
+            var billions = Math.floor((n % Math.pow(10, 12)) / Math.pow(10, 9));
+
+            var nAsText = '';
+            nAsText += _partOfNumberToBigElementOfPolishNumber(billions, ['miliard', 'miliardy', 'miliard\u00f3w'], true) + ' ';
+            nAsText += _partOfNumberToBigElementOfPolishNumber(millions, ['milion', 'miliony', 'milion\u00f3w'], true) + ' ';
+            nAsText += _partOfNumberToBigElementOfPolishNumber(thousands, ['tysi\u0105c', 'tysi\u0105ce', 'tysi\u0119cy'], true) + ' ';
+            nAsText += textHundreds[hundreds] + ' ';
+
+            var numberEndsWithTeens = (tens === 1 && units != 0);
+            if (numberEndsWithTeens)
+                nAsText += textTeens[units];
+            else
+                nAsText += textTens[tens] + ' ' + textUnits[units];
+
+            return _removeExtraSpaces(nAsText);
+        },
+
+        numberToPolishOrdinalNumber: function(n, gender=GENDER.MASCULINE) {
+            if (n === 0)
+                return _simpleChangePolishWordGender('zerowy', gender);
+            return _getTextNonOrdinalPartOfNumber(n) + ' ' + _getTextOrdinalPartOfNumber(n, gender);
+        },
+
+        numberToOrdinalNumber: function(n, language, gender=GENDER.MASCULINE) {
+            if (['pl', 'pl-pl', 'polish'].includes(language.toLowerCase()))
+                return numberToPolishOrdinalNumber(n, gender);
+            return n;
         },
 
         _prepareAltTexts: function($clone) {
@@ -234,6 +283,149 @@
 
         focusInvisibleElement: function() {
             $('#input_element_for_focus_to_change_focused_element_by_browser').focus();
+        },
+
+        _removeExtraSpaces: function(str) {
+            return str.replace(/\s+/g,' ').trim();
+        },
+
+        _reverseString: function(str) {
+            var newString = "";
+            for (var i = str.length - 1; i >= 0; i--) {
+                newString += str[i];
+            }
+            return newString;
+        },
+
+        _removeLastWord: function(str) {
+            var lastWordIndex = str.lastIndexOf(" ");
+            return str.substring(0, lastWordIndex);
+        },
+
+        _simpleChangePolishWordGender: function(word, gender) {
+            if (word === '' || ![GENDER.MASCULINE, GENDER.FEMININE, GENDER.NEUTER].includes(gender))
+                return word;
+
+            var genderCharacter = 'y';
+            if (gender === GENDER.FEMININE)
+                genderCharacter = 'a';
+            else if (gender === GENDER.NEUTER)
+                genderCharacter = 'e';
+
+            var lastCharacter = word.slice(word.length - 1);
+            var lastCharacterIsAGenderCharacter = ['a', 'e', 'y'].includes(lastCharacter);
+
+            if (lastCharacterIsAGenderCharacter)
+                return word.slice(0, -1) + genderCharacter;
+            return word + genderCharacter;
+        },
+
+        _partOfNumberToBigElementOfPolishNumber: function(n, wordsDescribingNumber, skipZero=false) {
+            if (n === 0 && skipZero)
+                return '';
+
+            var numberEndsWith2or3or4 = [2, 3, 4].includes(n % 10);
+
+            if (n === 1)
+                return ' ' + wordsDescribingNumber[0] + ' ';
+            else if (numberEndsWith2or3or4)
+                return numberToPolishNumber(n) + ' ' + wordsDescribingNumber[1] + ' ';
+            return numberToPolishNumber(n) + ' ' + wordsDescribingNumber[2] + ' ';
+        },
+
+        _getOrdinalPartOfNumber: function(n) {
+            var reversedOrdinalPart = '';
+
+            var nAsString = n.toString();
+            for (var i = nAsString.length - 1; i >= 0; i--) {
+                reversedOrdinalPart += nAsString[i];
+
+                if (nAsString[i] !== '0') {
+                    unitsJustAdded = ((nAsString.length - i - 1) % 3 === 0)
+                    if (unitsJustAdded && i > 0)
+                        reversedOrdinalPart += nAsString[i-1];
+                    break;
+                }
+            }
+
+            ordinalPart = _reverseString(reversedOrdinalPart);
+            return parseInt(ordinalPart);
+        },
+
+        _getTextNonOrdinalPartOfNumber: function(n) {
+            numberLength = n.toString().length;
+            var ordinalPartAsStr = _getOrdinalPartOfNumber(n).toString();
+            var ordinalPartLength = ordinalPartAsStr.length;
+            var numberWithoutOrdinalPart = Math.floor(n / Math.pow(10, ordinalPartLength)) * Math.pow(10, ordinalPartLength);
+
+            var hasNonOrdinalPart = (ordinalPartLength !== numberLength);
+            if (!hasNonOrdinalPart)
+                return '';
+            var nAsText = numberToPolishNumber(numberWithoutOrdinalPart);
+
+            var ordinalPartSize = Math.floor((ordinalPartLength - 1) / 3);
+            var nonOrdinalPartWithoutOrdinalPartSize = Math.floor(n / Math.pow(10, (ordinalPartSize + 1) * 3)) * Math.pow(10, (ordinalPartSize + 1) * 3);
+
+            var ordinalPartIsABigNumber = (ordinalPartLength > 3);
+            var nonOrdinalPartContainsOrdinalPartSize = (nonOrdinalPartWithoutOrdinalPartSize !== numberWithoutOrdinalPart);
+
+            if (ordinalPartIsABigNumber && nonOrdinalPartContainsOrdinalPartSize)
+                nAsText = _removeLastWord(nAsText);
+
+            return _removeExtraSpaces(nAsText)
+        },
+
+        _getTextOrdinalPartOfNumber: function(n, gender=GENDER.MASCULINE) {
+            var textUnits = ['', 'pierwszy', 'drugi', 'trzeci', 'czwarty', 'pi\u0105ty', 'sz\u00f3sty', 'si\u00f3dmy', '\u00f3smy', 'dziewi\u0105ty'];
+            var textTeens = ['', 'jedenasty', 'dwunasty', 'trzynasty', 'czternasty', 'pi\u0119tnasty', 'szesnasty', 'siedemnasty', 'osiemnasty', 'dziewi\u0119tnasty'];
+            var textTens = ['', 'dziesi\u0105ty', 'dwudziesty', 'trzydziesty', 'czterdziesty', 'pi\u0119\u0107dziesi\u0105ty', 'sze\u015b\u0107dziesi\u0105ty', 'siedemdziesi\u0105ty', 'osiemdziesi\u0105ty', 'dziewi\u0119\u0107dziesi\u0105ty'];
+            var textHundreds = ['', 'setny', 'dwusetny', 'trzysetny', 'czterysetny', 'pi\u0119\u0107setny', 'sze\u015b\u0107setny', 'siedemsetny', 'osiemsetny', 'dziewi\u0119\u0107setny'];
+
+            var textBigUnits = ['', 'jedno', 'dwu', 'trzy', 'cztero', 'pi\u0119cio', 'sze\u015bcio', 'siedmio', 'o\u015bmio', 'dziewi\u0119cio'];
+            var textBigTeens = ['', 'jedenasto', 'dwunasto', 'trzynasto', 'czternasto', 'pi\u0119tnasto', 'szesnasto', 'siedemnasto', 'osiemnasto', 'dziewi\u0119tnasto'];
+            var textBigTens = ['', 'dziesi\u0119cio', 'dwudziesto', 'trzydziesto', 'czterdziesto', 'pi\u0119\u0107dziesi\u0119cio', 'sze\u015b\u0107dziesi\u0119cio', 'siedemdziesi\u0119cio', 'osiemdziesi\u0119cio', 'dziewi\u0119\u0107dziesi\u0119cio'];
+            var textBigHundreds = ['', 'stu', 'dwustu', 'trzystu', 'czterystu', 'pi\u0119ciuset', 'sze\u015bciuset', 'siedmuset', 'o\u015bmiuset', 'dziewi\u0119ciuset'];
+            var sizes = ['', 'tysi\u0119czny', 'milionowy', 'miliardowy'];
+
+            [textUnits, textTeens, textTens, textHundreds, sizes].forEach((_, i, arr) => {
+                arr[i].forEach((str, j, arr) => arr[j] = _simpleChangePolishWordGender(str, gender));
+            });
+            if (gender === GENDER.FEMININE) {
+                textUnits[2] = 'druga';
+            }
+
+            var ordinalPart = _getOrdinalPartOfNumber(n);
+            var ordinalPartAsStr = ordinalPart.toString();
+            var ordinalPartLength = ordinalPartAsStr.length;
+
+            var ordinalPartIsUnits = ((ordinalPartLength - 1) % 3 === 0);
+            var ordinalPartIsTens = ((ordinalPartLength - 1) % 3 === 1);
+            var ordinalPartIsTeens = (ordinalPartIsTens && ordinalPartAsStr[0] === '1' && ordinalPartAsStr[1] !== '0');
+            var ordinalPartIsHundreds = ((ordinalPartLength - 1) % 3 === 2);
+            var size = sizes[Math.floor((ordinalPartLength - 1) / 3)];
+
+            if (ordinalPartLength === 1)
+                ordinalPartAsText = textUnits[ordinalPartAsStr[0]];
+            else if (ordinalPartIsTeens && ordinalPartLength === 2)
+                ordinalPartAsText = textTeens[ordinalPartAsStr[1]];
+            else if (ordinalPartLength === 2)
+                ordinalPartAsText = (textTens[ordinalPartAsStr[0]] + ' ' + textUnits[ordinalPartAsStr[1]]).trim();
+            else if (ordinalPartLength === 3)
+                ordinalPartAsText = textHundreds[ordinalPartAsStr[0]];
+            else {
+                if (ordinalPartIsUnits)
+                    ordinalPartAsText = textBigUnits[ordinalPartAsStr[0]];
+                else if (ordinalPartIsTeens)
+                    ordinalPartAsText = textBigTeens[ordinalPartAsStr[1]];
+                else if (ordinalPartIsTens)
+                    ordinalPartAsText = (textBigTens[ordinalPartAsStr[0]] + ' ' + textBigUnits[ordinalPartAsStr[1]]).trim();
+                else if (ordinalPartIsHundreds)
+                    ordinalPartAsText = textBigHundreds[ordinalPartAsStr[0]];
+
+                ordinalPartAsText += size;
+            }
+
+            return _removeExtraSpaces(ordinalPartAsText);
         }
 
     }
