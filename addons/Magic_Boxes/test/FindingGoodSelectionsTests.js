da5d960f36af453d6f0cda6e944dae3b10f0107f
jedrzej-niemiera <108787447+jedrzej-niemiera@users.noreply.github.com>
2022-08-11 12:42:25 +0200

    Add GSA functionality to Magic Boxes, re #8462 (#808)
    * Fix deploy bugs
    
    * Fix saving user selections bug
    
    Co-authored-by: MichalSawoniuk <msawoniuk@solwit.com>
    Co-authored-by: Michal-sw-solwit <100777673+Michal-sw-solwit@users.noreply.github.com>
    Co-authored-by: msawoniuk <79645404+msawoniuk@users.noreply.github.com>

--- addons/Magic_Boxes/test/FindingGoodSelectionsTests.js
+++ addons/Magic_Boxes/test/FindingGoodSelectionsTests.js
@@ -3,7 +3,7 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
         this.presenter = AddonMagic_Boxes_create();
     },
 
-    'test horizontal answer': function () {
+    'test given grid with horizontal answer when looking for the answer then the answer will be found': function () {
         var grid = [
             ['a', 'b', 'c'],
             ['k', 'o', 't'],
@@ -13,13 +13,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [true,  true,  true],
             [false, false, false]
         ], answers = ["kot"];
+        this.presenter.GSAcorrectAnswerLocations["kot"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test horizontal answer real example': function () {
+    'test given grid with realistic horizontal answer when looking for the answer then the answer will be found': function () {
         var grid = [
             ['s', 'w', 'i', 'o', 'w', 'k', 'l'],
             ['b', 'i', 'o', 'm', 'a', 's', 's'],
@@ -39,13 +43,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [false, false, false, false, false, false, false],
             [false, false, false, false, false, false, false]
         ], answers = ["biomass"];
+        this.presenter.GSAcorrectAnswerLocations["biomass"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test vertical answer': function () {
+    'test given grid with vertical answer when looking for the answer then the answer will be found': function () {
         var grid = [
             ['a', 'b', 'c'],
             ['k', 'o', 't'],
@@ -55,13 +63,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [false, true, false],
             [false, true, false]
         ], answers = ["boj"];
+        this.presenter.GSAcorrectAnswerLocations["boj"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test diagonal answer': function () {
+    'test given grid with diagonal answer when looking for the answer then the answer will be found': function () {
         var grid = [
             ['a', 'b', 'c'],
             ['k', 'o', 't'],
@@ -71,13 +83,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [false, true, false],
             [false, false, true]
         ], answers = ["aok"];
+        this.presenter.GSAcorrectAnswerLocations["aok"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test reverse diagonal answer': function () {
+    'test given grid with reverse diagonal answer when looking for the answer then the answer will be found': function () {
         var grid = [
             ['a', 'b', 'c'],
             ['k', 'o', 't'],
@@ -87,13 +103,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [false, true, false],
             [true, false, false]
         ], answers = ["ioc"];
+        this.presenter.GSAcorrectAnswerLocations["ioc"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test reverse diagonal answer not starting in corner': function () {
+    'test given grid with reverse diagonal answer not starting in a corner when looking for the answer then the answer will be found': function () {
         var grid = [
             ['a', 'b', 'c', 'd', 'e'],
             ['f', 'g', 'h', 'i', 'j'],
@@ -105,13 +125,17 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [false, false, true, false, false],
             [false, false, false, false, false]
         ], answers = ["mie"];
+        this.presenter.GSAcorrectAnswerLocations["mie"] = {
+            row: [],
+            column: [],
+        };
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test mixed answer': function () {
+    'test given grid with mixed answers when looking for the answers then the answers will be found': function () {
         var grid = [
             ['a', 'b', 'c', 'd', 'e'],
             ['f', 'g', 'h', 'i', 'j'],
@@ -123,14 +147,20 @@ TestCase("[Magic Boxes] Finding good selections in grid", {
             [true,  true,  false, true,  false],
             [true,  true,  true,  true,  false]
         ], answers = ["abc", "kr", "rs", "int", "plh"];
-
+        for (var i in answers) {
+            let ans = answers[i];
+            this.presenter.GSAcorrectAnswerLocations[ans] = {
+                row: [],
+                column: []
+            };
+        }
 
         var result = this.presenter.findGoodSelections(grid, answers);
 
         assertEquals(selections, result);
     },
 
-    'test answer not present in grid': function () {
+    'test given grid without correct answers when looking for answers then the answer will NOT be found': function () {
         var grid = [
             ['a', 'b', 'c'],
             ['k', 'o', 't'],
