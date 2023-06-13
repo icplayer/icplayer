5555d6264aa8a97baaf5dbaf836f77b01765a280
wkarasSolwit <87431460+wkarasSolwit@users.noreply.github.com>
2023-06-13 10:13:29 +0200

    feat(addons) render SVG as HTML in Double State Button #8756 (#971)
    * feat(addons) add renderSVGAsHTML property re #8756
    
    * correct importing SVG files
    
    ---------
    
    Co-authored-by: maciej-zawlocki-solwit <34648176+maciej-zawlocki-solwit@users.noreply.github.com>

--- addons/Double_State_Button/test/UpgradeModelValidation.js
+++ addons/Double_State_Button/test/UpgradeModelValidation.js
@@ -18,5 +18,42 @@ TestCase("[Double State Button] Upgrade model validation", {
         var upgradedModel = this.presenter.upgradeDisable(oldModel);
 
         assertEquals(expectedModel, upgradedModel);
+    },
+
+    'test given model without renderSVGAsHTML when addRenderSVGAsHTML was called then returns model with false renderSVGAsHTML value': function () {
+        const oldModel = {
+            Title: "Some text",
+            Image: "/file/server/123456",
+            onClick: "Empty script"
+        };
+        const expectedModel = {
+            Title: "Some text",
+            Image: "/file/server/123456",
+            onClick: "Empty script",
+            renderSVGAsHTML: "False"
+        };
+
+        const upgradedModel = this.presenter.addRenderSVGAsHTML(oldModel);
+
+        assertEquals(expectedModel, upgradedModel);
+    },
+
+    'test given model with renderSVGAsHTML on True when addRenderSVGAsHTML was called then returns model with not changed renderSVGAsHTML value': function () {
+        const oldModel = {
+            Title: "Some text",
+            Image: "/file/server/123456",
+            onClick: "Empty script",
+            renderSVGAsHTML: "True"
+        };
+        const expectedModel = {
+            Title: "Some text",
+            Image: "/file/server/123456",
+            onClick: "Empty script",
+            renderSVGAsHTML: "True"
+        };
+
+        const upgradedModel = this.presenter.addRenderSVGAsHTML(oldModel);
+
+        assertEquals(expectedModel, upgradedModel);
     }
 });
