ff9505fbc89a366905dad27c23fa76152c24ee64
MichalSawoniuk <msawoniuk@solwit.com>
2023-10-11 19:08:04 +0200

    Change new method to use seed from printableController

--- src/main/java/com/lorepo/icplayer/client/module/sourcelist/SourceListPrintable.java
+++ src/main/java/com/lorepo/icplayer/client/module/sourcelist/SourceListPrintable.java
@@ -1,18 +1,22 @@
 package com.lorepo.icplayer.client.module.sourcelist;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.lorepo.icf.utils.RandomUtils;
+import com.google.gwt.user.client.Random;
 import com.lorepo.icplayer.client.printable.PrintableContentParser;
+import com.lorepo.icplayer.client.printable.PrintableController;
 import com.lorepo.icplayer.client.printable.Printable.PrintableMode;
 
 public class SourceListPrintable {
 	
 	private SourceListModule model = null;
+	private PrintableController controller = null;
 	
-	public SourceListPrintable(SourceListModule model) {
+	public SourceListPrintable(SourceListModule model, PrintableController controller) {
 		this.model = model;
+		this.controller = controller;
 	}
 	
 	public String getPrintableHTML(String className, boolean showAnswers) {
@@ -34,14 +38,27 @@ public class SourceListPrintable {
 	
 	private List<Integer> getOrderedIndexes() {
 		List<Integer> orderedIndexes;
+		orderedIndexes = new ArrayList<Integer>();
+		for (int i = 0; i < model.getItemCount(); i++) {
+			orderedIndexes.add(i);
+		}
 		if (this.model.isRandomOrder()) {
-			orderedIndexes = RandomUtils.singlePermutation(model.getItemCount());
-		} else {
-			orderedIndexes = new ArrayList<Integer>();
-			for (int i = 0; i < model.getItemCount(); i++) {
-				orderedIndexes.add(i);
-			}
+			randomizeOrder(orderedIndexes);
 		}
 		return orderedIndexes;
+	}
+	
+	private void randomizeOrder(List<Integer> itemsIndexes) {
+		for(int index = 0; index < itemsIndexes.size(); index += 1) {
+			int secondElementIndex = index + nextInt(itemsIndexes.size() - index);
+			Collections.swap(itemsIndexes, index, secondElementIndex);
+		}
+	}
+	
+	private int nextInt(int upperBound) {
+		if (controller != null) {
+			return controller.nextInt(upperBound);
+		}
+		return Random.nextInt(upperBound);
 	}
 }
