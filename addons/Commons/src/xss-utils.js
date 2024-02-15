4c2a0d37929f133952a47493e960ddf62871087e
MichalSawoniuk <msawoniuk@solwit.com>
2024-02-15 16:01:37 +0100

    Fix images not loading in IOS when added as inner HTML, re #9106

--- addons/Commons/src/xss-utils.js
+++ addons/Commons/src/xss-utils.js
@@ -11,12 +11,24 @@
 
     /**
      Sanitize HTML, securing it against potential xss attacks
+
+     allow specific protocols handlers in URL attributes via regex (default is false, be careful, XSS risk)
+     by default only http, https, ftp, ftps, tel, mailto, callto, sms, cid and xmpp are allowed.
+     Default RegExp: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i;
+     Source: https://github.com/cure53/DOMPurify
+
+     RegExp was extended to allow:
+     * prefix "file" - for macOS and Windows
+     * prefix "capacitor" - for IOS
+
+     Android does not need an extended version, as it starts its URL with https.
+
      @method sanitize
 
-     @param {String} raw html to be sanitized
+     @param {String} html - raw HTML to be sanitized
     */
     xssUtils.sanitize = function xssUtils_sanitize(html) {
-        return DOMPurify.sanitize(html);
+        return DOMPurify.sanitize(html, {ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp|file|capacitor):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i});
     };
 
     window.xssUtils = xssUtils;
--- addons/Commons/test/xss-utils/sanitize-method.js
+++ addons/Commons/test/xss-utils/sanitize-method.js
@@ -23,4 +23,28 @@ TestCase("[Commons - XSS Utils] sanitize method", {
 
         assertEquals("<div>Hello World</div>", outputHTML);
     },
+
+    'test given img with inline js when sanitize is called then src attribute will be removed': function() {
+        var html = "<img src=\"javascript:alert(\"hello everybody\")\"/>";
+
+        var outputHTML = window.xssUtils.sanitize(html);
+
+        assertEquals("<img>", outputHTML);
+    },
+
+    'test given img with src starting with file when sanitize is called then do not remove src attribute': function() {
+        var html = "<img src=\"file:///Users/app/data-lessons/5189827790728132/pages/../resources/123\">";
+
+        var outputHTML = window.xssUtils.sanitize(html);
+
+        assertEquals(html, outputHTML);
+    },
+
+    'test given img with src starting with capacitor when sanitize is called then do not remove src attribute': function() {
+        var html = "<img src=\"capacitor://pec/_capacitor_file_/var/mobile/Containers/Data/Application/../resources/123\">";
+
+        var outputHTML = window.xssUtils.sanitize(html);
+
+        assertEquals(html, outputHTML);
+    },
 });
