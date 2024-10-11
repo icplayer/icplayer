03ddec82447a4b64ff90621d879f293d471f8e7c
maciej-zawlocki-solwit <34648176+maciej-zawlocki-solwit@users.noreply.github.com>
2024-10-11 12:34:21 +0200

    Added TTS support to Writing Calculations, re #PLA-5 (#1208)
    Co-authored-by: Maciej Zawłocki <mzawlocki@solwit.com>

--- addons/WritingCalculations/test/UpgradeModelTests.js
+++ addons/WritingCalculations/test/UpgradeModelTests.js
@@ -52,5 +52,94 @@ TestCase("[Writing Calculations] Upgrade Model Tests", {
         const upgradedModel = this.presenter.upgradeModel(this.model);
 
         assertEquals("True", upgradedModel["showAllAnswersInGSA"]);
+    },
+
+    "test given model without TTS properties when upgradeModel is called then TTS properties are added": function () {
+        const upgradedModel = this.presenter.upgradeModel(this.model);
+
+        assertNotUndefined(upgradedModel["speechTexts"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["Gap"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["Gap"]["Gap"]);
+        assertEquals("", upgradedModel["speechTexts"]["Gap"]["Gap"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["AdditionalGap"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["AdditionalGap"]["AdditionalGap"]);
+        assertEquals("", upgradedModel["speechTexts"]["AdditionalGap"]["AdditionalGap"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["Empty"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["Empty"]["Empty"]);
+        assertEquals("", upgradedModel["speechTexts"]["Empty"]["Empty"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["AdditionSign"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["AdditionSign"]["AdditionSign"]);
+        assertEquals("", upgradedModel["speechTexts"]["AdditionSign"]["AdditionSign"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["SubtractionSign"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["SubtractionSign"]["SubtractionSign"]);
+        assertEquals("", upgradedModel["speechTexts"]["SubtractionSign"]["SubtractionSign"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["DivisionSign"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["DivisionSign"]["DivisionSign"]);
+        assertEquals("", upgradedModel["speechTexts"]["DivisionSign"]["DivisionSign"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["MultiplicationSign"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["MultiplicationSign"]["MultiplicationSign"]);
+        assertEquals("", upgradedModel["speechTexts"]["MultiplicationSign"]["MultiplicationSign"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["EqualsSign"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["EqualsSign"]["EqualsSign"]);
+        assertEquals("", upgradedModel["speechTexts"]["EqualsSign"]["EqualsSign"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["DecimalSeparator"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["DecimalSeparator"]["DecimalSeparator"]);
+        assertEquals("", upgradedModel["speechTexts"]["DecimalSeparator"]["DecimalSeparator"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["Line"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["Line"]["Line"]);
+        assertEquals("", upgradedModel["speechTexts"]["Line"]["Line"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["Correct"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["Correct"]["Correct"]);
+        assertEquals("", upgradedModel["speechTexts"]["Correct"]["Correct"]);
+
+        assertNotUndefined(upgradedModel["speechTexts"]["Wrong"]);
+        assertNotUndefined(upgradedModel["speechTexts"]["Wrong"]["Wrong"]);
+        assertEquals("", upgradedModel["speechTexts"]["Wrong"]["Wrong"]);
+    },
+
+    "test given model with TTS properties when upgradeModel is called then TTS properties are not overwritten": function () {
+        this.model["speechTexts"] = {
+            Gap: {Gap: "gapa"},
+            AdditionalGap: {AdditionalGap: "dodatkowa gapa"},
+            Empty: {Empty: "pusty"},
+            AdditionSign: {AdditionSign: "dodawanie"},
+            SubtractionSign: {SubtractionSign: "odejmowanie"},
+            DivisionSign: {DivisionSign: "dzielenie"},
+            MultiplicationSign: {MultiplicationSign: "mnożenie"},
+            EqualsSign: {EqualsSign: "równanie"},
+            DecimalSeparator: {DecimalSeparator: "kropka"},
+            Line: {Line: "linia"},
+            Correct: {Correct: "poprawne"},
+            Wrong: {Wrong: "niepoprawne"}
+
+        };
+
+        const upgradedModel = this.presenter.upgradeModel(this.model);
+
+        assertEquals("gapa", upgradedModel["speechTexts"]["Gap"]["Gap"]);
+        assertEquals("dodatkowa gapa", upgradedModel["speechTexts"]["AdditionalGap"]["AdditionalGap"]);
+        assertEquals("pusty", upgradedModel["speechTexts"]["Empty"]["Empty"]);
+        assertEquals("dodawanie", upgradedModel["speechTexts"]["AdditionSign"]["AdditionSign"]);
+        assertEquals("odejmowanie", upgradedModel["speechTexts"]["SubtractionSign"]["SubtractionSign"]);
+        assertEquals("dzielenie", upgradedModel["speechTexts"]["DivisionSign"]["DivisionSign"]);
+        assertEquals("mnożenie", upgradedModel["speechTexts"]["MultiplicationSign"]["MultiplicationSign"]);
+        assertEquals("równanie", upgradedModel["speechTexts"]["EqualsSign"]["EqualsSign"]);
+        assertEquals("kropka", upgradedModel["speechTexts"]["DecimalSeparator"]["DecimalSeparator"]);
+        assertEquals("linia", upgradedModel["speechTexts"]["Line"]["Line"]);
+        assertEquals("poprawne", upgradedModel["speechTexts"]["Correct"]["Correct"]);
+        assertEquals("niepoprawne", upgradedModel["speechTexts"]["Wrong"]["Wrong"]);
     }
 });
