2d17ec69b1d0f340930e4328f171a5a032f94e50
MichalSawoniuk <msawoniuk@solwit.com>
2024-11-28 13:06:21 +0100

    Fix clean code issue

--- addons/Vimeo/src/presenter.js
+++ addons/Vimeo/src/presenter.js
@@ -3,21 +3,21 @@ function AddonVimeo_create(){
 	var presenter = function(){};
 
 	const ERROR_MESSAGES = {
-        INVALID_ID: "Invalid movie ID",
-    };
+		INVALID_ID: "Invalid movie ID"
+	};
 
 	presenter.run = function(view, model){
 		const src = `https://player.vimeo.com/video/${model["movie id"]}?title=0&byline=0&portrait=0`;
 		const sanitizedSrc = window.xssUtils.sanitize(src);
-
+		
 		const iframe = view.getElementsByTagName("iframe")[0];
 		iframe.setAttribute("src", sanitizedSrc);
 	};
-
+	
 	presenter.createPreview = function(view, model){
 		const src = `https://vimeo.com/api/v2/video/${model["movie id"]}.json`;
 		const sanitizedSrc = window.xssUtils.sanitize(src);
-
+		
 		fetch(sanitizedSrc, {method: "GET"})
 			.then(response => response.json())
 			.then(data => {
@@ -27,12 +27,12 @@ function AddonVimeo_create(){
 				showErrorMessage(view);
 		});
 	};
-
+	
 	function showErrorMessage (view) {
-        const errorContainer = '<p>' + ERROR_MESSAGES.INVALID_ID + '</p>';
-
-        $(view).html(errorContainer);
-    }
+		const errorContainer = '<p>' + ERROR_MESSAGES.INVALID_ID + '</p>';
+		
+		$(view).html(errorContainer);
+	}
 	
 	return presenter;
 }
