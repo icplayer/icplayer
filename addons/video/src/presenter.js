84eecf721d150d5cf477fded5f8353f1819d5186
maciej-zawlocki-solwit <34648176+maciej-zawlocki-solwit@users.noreply.github.com>
2025-02-14 13:10:52 +0100

    Fixed issue with hls files in the video addon when a page is reloaded, re #PLA-129 (#1266)
    Co-authored-by: Maciej Zawłocki <mzawlocki@solwit.com>

--- addons/video/src/presenter.js
+++ addons/video/src/presenter.js
@@ -504,7 +504,7 @@ function Addonvideo_create() {
         MathJax.Hub.signal.hooks["End Process"].Remove(presenter.mathJaxHook);
     };
 
-    presenter.destroy = function () {
+    presenter.onDestroy = function () {
         var view = document.getElementsByClassName('ic_page');
 
         if (presenter.hlsPlayer != null) {
@@ -1180,9 +1180,6 @@ function Addonvideo_create() {
             presenter.videoObject.addEventListener('playing', presenter.onVideoPlaying, false);
         }
         $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', presenter.fullscreenChangedEventReceived);
-
-        MutationObserverService.createDestroyObserver(presenter.configuration.addonID, presenter.destroy, presenter.$view.get(0));
-		MutationObserverService.setObserver();
 
         presenter.addClickListener();
     };
@@ -2379,3 +2376,7 @@ function Addonvideo_create() {
 
     return presenter;
 }
+
+Addonvideo_create.__supported_player_options__ = {
+    interfaceVersion: 2
+};
