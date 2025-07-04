3acaceae4248bf7de20bb4474cfb4658e64e9e57
Michal SAWONIUK <michal.sawoniuk@alten.com>
2025-07-04 08:47:33 +0200

    Fix clean code issues

--- src/test/java/com/lorepo/icplayer/client/module/imagegap/ImageGapPresenterResetTestCase.java
+++ src/test/java/com/lorepo/icplayer/client/module/imagegap/ImageGapPresenterResetTestCase.java
@@ -8,60 +8,61 @@ import org.mockito.Mockito;
 
 public class ImageGapPresenterResetTestCase {
     private PlayerServicesMockup services;
-	private ImageGapViewMockup display;
+    private ImageGapViewMockup display;
     private ImageGapModule module;
     private ImageGapPresenter presenterSpy;
-
+    
     @Before
     public void setUp() {
         services = new PlayerServicesMockup();
         module = new ImageGapModule();
-		display = new ImageGapViewMockup(module);
+        display = new ImageGapViewMockup(module);
     }
-
+    
     private void buildPresenter() {
         ImageGapPresenter presenter = new ImageGapPresenter(module, services);
         presenter.addView(display);
         presenterSpy = Mockito.spy(presenter);
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenCallingShowThenCallsShowInView() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.reset(false);
-
+        
         Mockito.verify(presenterSpy, Mockito.times(1)).show();
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenResettingModuleThenDoesNotCallHide() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.reset(false);
-
+        
         Mockito.verify(presenterSpy, Mockito.never()).hide();
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenResettingModuleThenCallsHide() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.reset(false);
-
+        
         Mockito.verify(presenterSpy, Mockito.times(1)).hide();
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenResettingModuleThenDoesNotCallShow() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.reset(false);
-
+        
         Mockito.verify(presenterSpy, Mockito.never()).show();
     }
+
 }
--- src/test/java/com/lorepo/icplayer/client/module/imagegap/ImageGapPresenterShowAndHideTestCase.java
+++ src/test/java/com/lorepo/icplayer/client/module/imagegap/ImageGapPresenterShowAndHideTestCase.java
@@ -14,111 +14,111 @@ import com.lorepo.icplayer.client.module.imagegap.mockup.ImageGapViewMockup;
 public class ImageGapPresenterShowAndHideTestCase {
     private PlayerServicesMockup services;
     private ImageGapModule module;
-	private ImageGapViewMockup display;
+    private ImageGapViewMockup display;
     private ImageGapPresenter presenterSpy;
-
+    
     @Before
     public void setUp() {
         services = new PlayerServicesMockup();
         module = new ImageGapModule();
         display = new ImageGapViewMockup(module);
     }
-
+    
     private void buildPresenter() {
         ImageGapPresenter presenter = new ImageGapPresenter(module, services);
         presenter.addView(display);
         presenterSpy = Mockito.spy(presenter);
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenCallingShowThenCallsShowInViewObject() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.show();
-
+        
         assertTrue(display.isVisible());
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenCallingHideThenCallsHideInViewObject() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.hide();
-
+        
         assertFalse(display.isVisible());
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenCallingShowThenCallsShowInViewObject() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.show();
-
+        
         assertTrue(display.isVisible());
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenCallingHideThenCallsHideInViewObject() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.hide();
-
+        
         assertFalse(display.isVisible());
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenCallingShowThenStateIsVisibleIsTrue() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.show();
-
+        
         String stateObj = presenterSpy.getState();
         IJsonServices json = services.getJsonServices();
-		HashMap<String, String> state = json.decodeHashMap(stateObj);
+        HashMap<String, String> state = json.decodeHashMap(stateObj);
         assertTrue(state.get("isVisible").equals("true"));
     }
-
+    
     @Test
     public void givenVisibleByDefaultModuleWhenCallingHideThenStateIsVisibleIsFalse() {
         module.setIsVisible(true);
         buildPresenter();
-
+        
         presenterSpy.hide();
-
+        
         String stateObj = presenterSpy.getState();
         IJsonServices json = services.getJsonServices();
-		HashMap<String, String> state = json.decodeHashMap(stateObj);
+        HashMap<String, String> state = json.decodeHashMap(stateObj);
         assertTrue(state.get("isVisible").equals("false"));
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenCallingShowThenStateIsVisibleIsTrue() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.show();
-
+        
         String stateObj = presenterSpy.getState();
         IJsonServices json = services.getJsonServices();
-		HashMap<String, String> state = json.decodeHashMap(stateObj);
+        HashMap<String, String> state = json.decodeHashMap(stateObj);
         assertTrue(state.get("isVisible").equals("true"));
     }
-
+    
     @Test
     public void givenNotVisibleByDefaultModuleWhenCallingHideThenStateIsVisibleIsFalse() {
         module.setIsVisible(false);
         buildPresenter();
-
+        
         presenterSpy.hide();
-
+        
         String stateObj = presenterSpy.getState();
         IJsonServices json = services.getJsonServices();
-		HashMap<String, String> state = json.decodeHashMap(stateObj);
+        HashMap<String, String> state = json.decodeHashMap(stateObj);
         assertTrue(state.get("isVisible").equals("false"));
     }
 
