0a08c5a24a059f13ac2988174063d4303150b11b
maciej-zawlocki-solwit <34648176+maciej-zawlocki-solwit@users.noreply.github.com>
2026-03-20 12:03:13 +0100

    Fixed Table checking correct dropdown answer as incorrect if it includes alt text, re #PLA-269 (#1353)
    Co-authored-by: Maciej Zawlocki <mzawlocki@MacBook-Pro-Maciej.local>

--- addons/Table/test/ScoringTests.js
+++ addons/Table/test/ScoringTests.js
@@ -23,9 +23,11 @@ TestCase("[Table] GetScore / GetMaxScore / GetErrorCount", {
         this.presenter.textParser = {
             parseAltTexts: sinon.stub(),
             parse: sinon.stub(),
+            parseAnswer: sinon.stub(),
         };
         this.presenter.textParser.parse.returnsArg(0);
         this.presenter.textParser.parseAltTexts.returnsArg(0);
+        this.presenter.textParser.parseAnswer.returnsArg(0);
     },
 
     tearDown: function () {
@@ -196,82 +198,30 @@ TestCase("[Table] GetScore / GetMaxScore / GetErrorCount", {
         assertEquals(8, this.presenter.getMaxScore());
         assertEquals(6, this.presenter.getScore());
         assertEquals(1, this.presenter.getErrorCount());
+    },
+
+    'test select gap - given answer containing an alt text, when onEdit is called, set correct value': function () {
+        let gap1 = new this.presenter.SelectGap("gapId", ["\\alt{hello|world}","wrong answer"], 1);
+        gap1.$view = {
+            find: (query)=>{
+                if (query === ':selected') {
+                    return {
+                        val: ()=>{return "\\alt{hello|world}";},
+                        text: () => {return "hello";}
+                    }
+                } else {
+                    return {
+                        val: ()=>{return "wrong answer";},
+                        text: ()=>{return "wrong answer";}
+                    }
+                }
+            }
+        }
+        let notifyEditStub = sinon.stub(gap1, "notifyEdit");
+        let notifyStub = sinon.stub(gap1, "notify");
+
+        gap1.onEdit();
+
+        assertEquals("\\alt{hello|world}", gap1.value);
     }
 });
-//
-//TestCase("[Table] GetMaxScore / GetScore / GetErrorCount - is not activity option selected", {
-//    setUp: function () {
-//        this.presenter = AddonTable_create();
-//        this.presenter.eventBus = function () {};
-//
-//        this.presenter.configuration = {
-//            isActivity: true,
-//            isNotActivity: false,
-//            isCaseSensitive: false,
-//            isPunctuationIgnored: false,
-//            addonID: "Table1"
-//        };
-//
-//        this.presenter.gapsContainer = new this.presenter.GapsContainerObject();
-//        this.stubs = {
-//            editableCreateView: sinon.stub(this.presenter.EditableInputGap.prototype, 'createView'),
-//            editableConnectEvents: sinon.stub(this.presenter.EditableInputGap.prototype, 'connectEvents'),
-//            draggableCreateView: sinon.stub(this.presenter.DraggableDroppableGap.prototype, 'createView'),
-//            draggableConnectEvents: sinon.stub(DraggableDroppableObject._internal, 'connectEvents'),
-//            setGapWidth: sinon.stub(this.presenter.GapUtils.prototype, 'setGapWidth')
-//        };
-//    },
-//
-//    tearDown: function () {
-//        this.presenter.EditableInputGap.prototype.createView.restore();
-//        this.presenter.EditableInputGap.prototype.connectEvents.restore();
-//        this.presenter.GapUtils.prototype.setGapWidth.restore();
-//        this.presenter.DraggableDroppableGap.prototype.createView.restore();
-//        DraggableDroppableObject._internal.connectEvents.restore();
-//    },
-//
-//    'test single empty gap filled': function () {
-//        this.presenter.configuration.isActivity = false;
-//        this.presenter.configuration.isNotActivity = true;
-//
-//        var gap = new this.presenter.DraggableDroppableGap("Table1-1", ["some value"], 1);
-//        gap.setValue("");
-//
-//        this.presenter.gapsContainer.addGap(gap);
-//
-//        assertEquals(0, this.presenter.getMaxScore());
-//        assertEquals(0, this.presenter.getScore());
-//        assertEquals(0, this.presenter.getErrorCount());
-//    },
-//
-//    'test multiple gaps - each filled differently': function () {
-//        this.presenter.configuration.isActivity = false;
-//        this.presenter.configuration.isNotActivity = true;
-//
-//
-//        var gap1 = new this.presenter.EditableInputGap("Table1-1", [""], 1);
-//        gap1.setValue('');
-//
-//        var gap2 = new this.presenter.EditableInputGap("Table1-2", ["ans1"], 1);
-//        gap2.setValue('ans1');
-//
-//        var gap3 = new this.presenter.EditableInputGap("Table1-3", ["some"], 1);
-//        gap3.setValue('some value');
-//
-//        var gap4 = new this.presenter.EditableInputGap("Table1-4", ["answ1", "answ2", "answ3"], 1);
-//        gap4.setValue('');
-//
-//        var gap5 = new this.presenter.EditableInputGap("Table1-5", ["answ1", "answ2"], 1);
-//        gap5.setValue('answ2');
-//
-//        this.presenter.gapsContainer.addGap(gap1);
-//        this.presenter.gapsContainer.addGap(gap2);
-//        this.presenter.gapsContainer.addGap(gap3);
-//        this.presenter.gapsContainer.addGap(gap4);
-//        this.presenter.gapsContainer.addGap(gap5);
-//
-//        assertEquals(0, this.presenter.getMaxScore());
-//        assertEquals(0, this.presenter.getScore());
-//        assertEquals(0, this.presenter.getErrorCount());
-//    }
-//});
