a53535f5a9bebaf8f4dcfbb3509a55c3a3b8c385
Michal SAWONIUK <michal.sawoniuk@alten.com>
2026-05-11 08:59:04 +0200

    Fix error in console

--- src/main/java/com/lorepo/icplayer/client/PlayerController.java
+++ src/main/java/com/lorepo/icplayer/client/PlayerController.java
@@ -684,13 +684,16 @@ public class PlayerController implements IPlayerController {
 	@Override
 	public int getIframeScroll() {
 		// when true, iframeScroll set by parsing message
-		if (isIframeInCrossDomain || !$wnd.window.top) {
+		if (isIframeInCrossDomain) {
 			return this.iframeScroll;
 		}
-		return this.getScrollTop();
+		return this.getScrollTop(this);
 	}
 	
-	private native int getScrollTop() /*-{
+	private native int getScrollTop(PlayerController x) /*-{
+		if (!$wnd.window.top) {
+		    return x.@com.lorepo.icplayer.client.PlayerController::iframeScroll;
+		}
 		var scrollTop = $wnd.window.top.pageYOffset;
 		if (scrollTop === 0) {
 			var contentView = $wnd.document.getElementById('content-view');
